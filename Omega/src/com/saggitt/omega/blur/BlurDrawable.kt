a0d7a41ba4ec173b5344e7c0e364aff0f60f2382
Saul Henriquez <saul_henriquez@hotmail.com>
2020-06-29 23:13:05 -0600

    add blur
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

---
+++ Omega/src/com/saggitt/omega/blur/BlurDrawable.kt
@@ -0,0 +1,51 @@
+/*
+ *     Copyright (C) 2019 Lawnchair Team.
+ *
+ *     This file is part of Lawnchair Launcher.
+ *
+ *     Lawnchair Launcher is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Lawnchair Launcher is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Lawnchair Launcher.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.saggitt.omega.blur
+
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+
+abstract class BlurDrawable : Drawable(), BlurWallpaperProvider.Listener {
+
+    abstract var blurRadii: Radii
+    abstract var viewOffsetX: Float
+
+    override fun draw(canvas: Canvas) {
+        draw(canvas, false)
+    }
+
+    abstract fun draw(canvas: Canvas, noRadius: Boolean = false)
+
+    abstract fun setBlurBounds(left: Float, top: Float, right: Float, bottom: Float)
+
+    abstract fun startListening()
+    abstract fun stopListening()
+
+    data class Radii(
+            val topLeft: Float = 0f,
+            val topRight: Float = 0f,
+            val bottomLeft: Float = 0f,
+            val bottomRight: Float = 0f) {
+
+        constructor(radius: Float) : this(radius, radius, radius, radius)
+        constructor(topRadius: Float, bottomRadius: Float)
+                : this(topRadius, topRadius, bottomRadius, bottomRadius)
+    }
+}
---
+++ Omega/src/com/saggitt/omega/blur/WallpaperFilter.kt
@@ -0,0 +1,80 @@
+/*
+ *
+ *  *
+ *  *  * Copyright (c) 2020 Omega Launcher
+ *  *  *
+ *  *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  *  * you may not use this file except in compliance with the License.
+ *  *  * You may obtain a copy of the License at
+ *  *  *
+ *  *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *  *
+ *  *  * Unless required by applicable law or agreed to in writing, software
+ *  *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *  * See the License for the specific language governing permissions and
+ *  *  * limitations under the License.
+ *  *
+ *
+ */
+
+package com.saggitt.omega.blur
+
+import android.graphics.Bitmap
+import com.saggitt.omega.OmegaPreferences
+
+interface WallpaperFilter {
+
+    fun applyPrefs(prefs: OmegaPreferences)
+
+    fun apply(wallpaper: Bitmap): ApplyTask
+
+    class ApplyTask {
+
+        val emitter = Emitter()
+
+        private var result: Bitmap? = null
+        private var error: Throwable? = null
+
+        private var callback: ((Bitmap?, Throwable?) -> Unit)? = null
+
+        fun setCallback(callback: (Bitmap?, Throwable?) -> Unit): ApplyTask {
+            result?.let {
+                callback(it, null)
+                return this
+            }
+            error?.let {
+                callback(null, it)
+                return this
+            }
+            this.callback = callback
+            return this
+        }
+
+        inner class Emitter {
+
+            fun onSuccess(result: Bitmap) {
+                callback?.let {
+                    it(result, null)
+                    return
+                }
+                this@ApplyTask.result = result
+            }
+
+            fun onError(error: Throwable) {
+                callback?.let {
+                    it(null, error)
+                    return
+                }
+                this@ApplyTask.error = error
+            }
+        }
+
+        companion object {
+
+            inline fun create(source: (Emitter) -> Unit): ApplyTask {
+                return ApplyTask().also { source(it.emitter) }
+            }
+        }
+    }
+}
