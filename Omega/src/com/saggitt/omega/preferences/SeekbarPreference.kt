f54dbe7a12a271be0618c3d25586555aab8d0976
Saul Henriquez <saul_henriquez@hotmail.com>
2020-11-14 13:35:30 -0600

    restore desktop icon scale
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

---
+++ Omega/src/com/saggitt/omega/preferences/SeekbarPreference.kt
@@ -0,0 +1,160 @@
+/*
+ *  Copyright (c) 2020 Omega Launcher
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package com.saggitt.omega.preferences
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.res.ColorStateList
+import android.util.AttributeSet
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.SeekBar
+import android.widget.TextView
+import androidx.preference.Preference
+import androidx.preference.PreferenceViewHolder
+import com.android.launcher3.R
+import com.android.launcher3.Utilities
+
+open class SeekbarPreference
+@JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0)
+    : Preference(context, attrs, defStyleAttr), SeekBar.OnSeekBarChangeListener, View.OnCreateContextMenuListener,
+        MenuItem.OnMenuItemClickListener {
+
+    private var mSeekbar: SeekBar? = null
+
+    @JvmField
+    var mValueText: TextView? = null
+
+    @JvmField
+    var min: Float = 0.toFloat()
+
+    @JvmField
+    var max: Float = 0.toFloat()
+
+    @JvmField
+    var current: Float = 0.toFloat()
+
+    @JvmField
+    var defaultValue: Float = 0.toFloat()
+    private var multiplier: Int = 0
+    private var format: String? = null
+
+    @JvmField
+    var steps: Int = 100
+    private var lastPersist = Float.NaN
+
+    open val allowResetToDefault = true
+
+    init {
+        layoutResource = R.layout.preference_seekbar
+        init(context, attrs!!)
+    }
+
+    private fun init(context: Context, attrs: AttributeSet) {
+        val ta = context.obtainStyledAttributes(attrs, R.styleable.SeekbarPreference)
+        min = ta.getFloat(R.styleable.SeekbarPreference_minValue, 0f)
+        max = ta.getFloat(R.styleable.SeekbarPreference_maxValue, 100f)
+        multiplier = ta.getInt(R.styleable.SeekbarPreference_summaryMultiplier, 1)
+        format = ta.getString(R.styleable.SeekbarPreference_summaryFormat)
+        defaultValue = ta.getFloat(R.styleable.SeekbarPreference_defaultSeekbarValue, min)
+        steps = ta.getInt(R.styleable.SeekbarPreference_steps, 100)
+        if (format == null) {
+            format = "%.2f"
+        }
+        ta.recycle()
+    }
+
+    override fun onBindViewHolder(holder: PreferenceViewHolder) {
+        super.onBindViewHolder(holder)
+        val view = holder.itemView
+        mSeekbar = view.findViewById(R.id.seekbar)
+        mValueText = view.findViewById(R.id.txtValue)
+        mSeekbar!!.max = steps
+
+        mSeekbar!!.setOnSeekBarChangeListener(this)
+        val stateList = ColorStateList.valueOf(Utilities.getOmegaPrefs(context).accentColor)
+        mSeekbar!!.thumbTintList = stateList
+        mSeekbar!!.progressTintList = stateList
+        mSeekbar!!.progressBackgroundTintList = stateList
+
+        current = getPersistedFloat(defaultValue)
+        val progress = ((current - min) / ((max - min) / steps))
+        mSeekbar!!.progress = Math.round(progress)
+
+        if (allowResetToDefault) view.setOnCreateContextMenuListener(this)
+        updateSummary()
+    }
+
+    fun setValue(value: Float) {
+        current = value
+        persistFloat(value)
+        updateDisplayedValue()
+    }
+
+    protected open fun updateDisplayedValue() {
+        mSeekbar?.setOnSeekBarChangeListener(null)
+        val progress = ((current - min) / ((max - min) / steps))
+        mSeekbar!!.progress = Math.round(progress)
+        updateSummary()
+        mSeekbar?.setOnSeekBarChangeListener(this)
+    }
+
+    override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+        current = min + (max - min) / steps * progress
+        current = Math.round(current * 100f) / 100f //round to .00 places
+        updateSummary()
+
+        persistFloat(current)
+    }
+
+    @SuppressLint("SetTextI18n")
+    protected open fun updateSummary() {
+        if (format != "%.2f") {
+            mValueText!!.text = String.format(format!!, current * multiplier)
+        } else {
+            mValueText!!.text = (current * multiplier).toInt().toString() + "dp"
+        }
+    }
+
+    override fun onStartTrackingTouch(seekBar: SeekBar) {}
+
+    override fun onStopTrackingTouch(seekBar: SeekBar) {
+        persistFloat(current)
+    }
+
+    override fun persistFloat(value: Float): Boolean {
+        if (value == lastPersist) return true
+        lastPersist = value
+        return super.persistFloat(value)
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu, v: View, menuInfo: ContextMenu.ContextMenuInfo?) {
+        menu.setHeaderTitle(title)
+        menu.add(0, 0, 0, R.string.reset_to_default)
+        for (i in (0 until menu.size())) {
+            menu.getItem(i).setOnMenuItemClickListener(this)
+        }
+    }
+
+    override fun onMenuItemClick(item: MenuItem): Boolean {
+        setValue(defaultValue)
+        return true
+    }
+}
+
