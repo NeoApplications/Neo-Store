165e7676a97cb57750d23c26daaa3429b61cbca9
Saul Henriquez <saul_henriquez@hotmail.com>
2021-02-13 11:49:16 -0600

    set correct package for apply_icons
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

--- iconloaderlib/src/com/android/launcher3/icons/ColorExtractor.java
+++ iconloaderlib/src/com/android/launcher3/icons/ColorExtractor.java
@@ -16,10 +16,16 @@
 package com.android.launcher3.icons;
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.util.SparseArray;
 
 import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Utility class for extracting colors from a bitmap.
@@ -124,6 +130,54 @@ public class ColorExtractor {
             }
         }
         return bestColor;
+    }
+
+    public static boolean isSingleColor(Drawable drawable, int color) {
+        if (drawable == null) return true;
+        final int testColor = posterize(color);
+        if (drawable instanceof ColorDrawable) {
+            return posterize(((ColorDrawable) drawable).getColor()) == testColor;
+        }
+        final Bitmap bitmap = drawableToBitmap(drawable);
+        if (bitmap == null) {
+            return false;
+        }
+        final int height = bitmap.getHeight();
+        final int width = bitmap.getWidth();
+
+        int[] pixels = new int[height * width];
+        bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
+        Set<Integer> set = Arrays.stream(pixels).boxed().collect(Collectors.toSet());
+        Integer[] distinctPixels = new Integer[set.size()];
+        set.toArray(distinctPixels);
+
+        for (int pixel : distinctPixels) {
+            if (testColor != posterize(pixel)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Copy pasted from Utilities
+    private static Bitmap drawableToBitmap(Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+
+        if (drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
+            return null;
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(
+                drawable.getIntrinsicWidth(),
+                drawable.getIntrinsicHeight(),
+                Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
     }
 
     private static final int MAGIC_NUMBER = 25;
