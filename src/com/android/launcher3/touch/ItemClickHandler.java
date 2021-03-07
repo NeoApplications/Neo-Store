65cc7472dfe57dfe66b35a1dab149b822e7db419
Saul Henriquez <saul_henriquez@hotmail.com>
2021-03-06 21:56:38 -0600

    corregir sort most used
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

--- src/com/android/launcher3/touch/ItemClickHandler.java
+++ src/com/android/launcher3/touch/ItemClickHandler.java
@@ -15,16 +15,6 @@
  */
 package com.android.launcher3.touch;
 
-import static com.android.launcher3.Launcher.REQUEST_BIND_PENDING_APPWIDGET;
-import static com.android.launcher3.Launcher.REQUEST_RECONFIGURE_APPWIDGET;
-import static com.android.launcher3.logging.StatsLogManager.LauncherEvent.LAUNCHER_FOLDER_OPEN;
-import static com.android.launcher3.model.AppLaunchTracker.CONTAINER_ALL_APPS;
-import static com.android.launcher3.model.data.ItemInfoWithIcon.FLAG_DISABLED_BY_PUBLISHER;
-import static com.android.launcher3.model.data.ItemInfoWithIcon.FLAG_DISABLED_LOCKED_USER;
-import static com.android.launcher3.model.data.ItemInfoWithIcon.FLAG_DISABLED_QUIET_USER;
-import static com.android.launcher3.model.data.ItemInfoWithIcon.FLAG_DISABLED_SAFEMODE;
-import static com.android.launcher3.model.data.ItemInfoWithIcon.FLAG_DISABLED_SUSPENDED;
-
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.pm.LauncherApps;
@@ -62,6 +52,17 @@ import com.android.launcher3.widget.PendingAppWidgetHostView;
 import com.android.launcher3.widget.WidgetAddFlowHandler;
 import com.android.launcher3.widget.WidgetManagerHelper;
 import com.saggitt.omega.util.Config;
+import com.saggitt.omega.util.DbHelper;
+
+import static com.android.launcher3.Launcher.REQUEST_BIND_PENDING_APPWIDGET;
+import static com.android.launcher3.Launcher.REQUEST_RECONFIGURE_APPWIDGET;
+import static com.android.launcher3.logging.StatsLogManager.LauncherEvent.LAUNCHER_FOLDER_OPEN;
+import static com.android.launcher3.model.AppLaunchTracker.CONTAINER_ALL_APPS;
+import static com.android.launcher3.model.data.ItemInfoWithIcon.FLAG_DISABLED_BY_PUBLISHER;
+import static com.android.launcher3.model.data.ItemInfoWithIcon.FLAG_DISABLED_LOCKED_USER;
+import static com.android.launcher3.model.data.ItemInfoWithIcon.FLAG_DISABLED_QUIET_USER;
+import static com.android.launcher3.model.data.ItemInfoWithIcon.FLAG_DISABLED_SAFEMODE;
+import static com.android.launcher3.model.data.ItemInfoWithIcon.FLAG_DISABLED_SUSPENDED;
 
 /**
  * Class for handling clicks on workspace and all-apps items
@@ -296,6 +297,12 @@ public class ItemClickHandler {
         if (v != null && launcher.getAppTransitionManager().supportsAdaptiveIconAnimation()) {
             // Preload the icon to reduce latency b/w swapping the floating view with the original.
             FloatingIconView.fetchIcon(launcher, v, item, true /* isOpening */);
+        }
+        if (item instanceof AppInfo) {
+            Log.i(TAG, "Clicking App " + item.title);
+            DbHelper db = new DbHelper(launcher.getApplicationContext());
+            db.updateAppCount(((AppInfo) item).componentName.getPackageName());
+            db.close();
         }
         launcher.startActivitySafely(v, intent, item, sourceContainer);
     }
