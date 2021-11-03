3aca3252be32ecc038032c768dca0363eefade85
Saul Henriquez <saul_henriquez@hotmail.com>
2021-11-03 11:15:04 -0600

    Add Compose theme
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

---
+++ Omega/src/com/saggitt/omega/theme/OmegaAppTheme.kt
@@ -0,0 +1,72 @@
+/*
+ *  This file is part of Omega Launcher
+ *  Copyright (c) 2021   Omega Launcher Team
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.saggitt.omega.theme
+
+import android.os.Build
+import androidx.compose.foundation.LocalIndication
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.platform.LocalContext
+
+@Composable
+fun OmegaAppTheme(
+    isDarkTheme: Boolean = isSystemInDarkTheme(),
+    isDynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val dynamicColor = isDynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    val myColorScheme = when {
+        dynamicColor && isDarkTheme -> {
+            dynamicDarkColorScheme(LocalContext.current)
+        }
+        dynamicColor && !isDarkTheme -> {
+            dynamicLightColorScheme(LocalContext.current)
+        }
+        isDarkTheme -> AppDarkColorScheme
+        else -> AppLightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = myColorScheme,
+        typography = AppTypography
+    ) {
+        val rippleIndication = rememberRipple()
+        CompositionLocalProvider(
+            LocalIndication provides rippleIndication,
+            content = content
+        )
+    }
+}
+
+private val AppDarkColorScheme = darkColorScheme(
+    primary = Purple200,
+    secondary = Teal200,
+    onBackground = Grey600
+)
+
+private val AppLightColorScheme = lightColorScheme(
+    primary = Purple500,
+    secondary = Teal200,
+    onBackground = Grey600
+)
+
+
---
+++ Omega/src/com/saggitt/omega/theme/Typography.kt
@@ -0,0 +1,44 @@
+/*
+ *  This file is part of Omega Launcher
+ *  Copyright (c) 2021   Omega Launcher Team
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.saggitt.omega.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.android.launcher3.R
+
+private val kaushanScript = FontFamily(
+    Font(R.font.kaushanscript_regular)
+)
+
+val AppTypography = Typography(
+    labelLarge = TextStyle(
+        fontFamily = kaushanScript,
+        fontWeight = FontWeight.Normal,
+        fontSize = 30.sp
+    ),
+
+    labelSmall = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    )
+)
