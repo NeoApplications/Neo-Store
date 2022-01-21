689dfe248619b64c5e38adb6f44f42e141203f5c
Saul Henriquez <saul_henriquez@hotmail.com>
2022-01-21 09:46:06 -0600

    Rework IconPackPreference
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

--- Omega/src/com/saggitt/omega/preferences/views/IconPackFragment.kt
+++ Omega/src/com/saggitt/omega/preferences/views/IconPackFragment.kt
@@ -18,15 +18,50 @@
 
 package com.saggitt.omega.preferences.views
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.RadioButton
+import androidx.compose.material.RadioButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import com.android.launcher3.R
+import com.android.launcher3.Utilities
 import com.android.launcher3.databinding.FragmentIconPackBinding
+import com.google.accompanist.drawablepainter.rememberDrawablePainter
+import com.saggitt.omega.iconpack.IconPackProvider
+import com.saggitt.omega.theme.OmegaAppTheme
+import com.saggitt.omega.theme.OmegaTheme
+import com.saggitt.omega.ui.component.ListItemWithIcon
+import com.saggitt.omega.ui.component.PreferenceItem
+import com.saggitt.omega.util.recreate
 
-class IconPackFragment : Fragment() {
+class IconPackFragment : Fragment(), SharedPreferences.OnSharedPreferenceChangeListener {
     lateinit var binding: FragmentIconPackBinding
 
     override fun onCreateView(
@@ -35,11 +70,123 @@ class IconPackFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View {
         binding = FragmentIconPackBinding.inflate(inflater, container, false)
+        binding.installedPacks.apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                OmegaAppTheme {
+                    IconPackList()
+                }
+            }
+
+        }
+
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Utilities.getPrefs(requireContext()).registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Utilities.getPrefs(requireContext()).unregisterOnSharedPreferenceChangeListener(this)
     }
 
     override fun onResume() {
         super.onResume()
         requireActivity().title = requireActivity().getString(R.string.icon_pack)
+    }
+
+    override fun onSharedPreferenceChanged(sharedPrefs: SharedPreferences?, key: String) {
+        if (key == "pref_icon_pack_package") {
+            this@IconPackFragment.recreate()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun IconPackList() {
+    val context = LocalContext.current
+    val prefs = Utilities.getOmegaPrefs(context)
+    val packs = IconPackProvider.INSTANCE.get(context).getIconPackList()
+
+    val colors = RadioButtonDefaults.colors(
+            selectedColor = Color(prefs.accentColor),
+            unselectedColor = Color.Gray
+    )
+
+    val (selectedOption, onOptionSelected) = remember {
+        mutableStateOf(prefs.iconPackPackage)
+    }
+    Column {
+
+        LazyColumn {
+            itemsIndexed(packs) { _, item ->
+                ListItemWithIcon(
+                        title = { Text(text = item.name, color = OmegaTheme.colors.textPrimary) },
+                        modifier = Modifier.clickable {
+                            prefs.iconPackPackage = item.packageName
+                            onOptionSelected(item.packageName)
+                        },
+                        description = {
+                            if (prefs.showDebugInfo)
+                                Text(text = item.packageName, color = OmegaTheme.colors.textSecondary)
+                        },
+                        startIcon = {
+                            Image(
+                                    painter = rememberDrawablePainter(drawable = item.icon),
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                            .clip(CircleShape)
+                                            .size(36.dp)
+                                            .background(
+                                                    MaterialTheme.colors.onBackground.copy(alpha = 0.12F)
+                                            )
+                            )
+                        },
+                        endCheckbox = {
+                            RadioButton(
+                                    selected = (item.packageName == selectedOption),
+                                    onClick = {
+                                        prefs.iconPackPackage = item.packageName
+                                        onOptionSelected(item.packageName)
+                                    },
+                                    colors = colors
+                            )
+                        },
+                        verticalPadding = 8.dp
+                )
+            }
+        }
+
+        PreferenceItem(
+                title = {
+                    Text(text = stringResource(id = R.string.get_more_icon_packs), color = OmegaTheme.colors.textPrimary)
+                },
+                modifier = Modifier
+                        .clickable {
+                            val intent = Intent.parseUri(context.getString(R.string.market_search_intent), 0)
+                            intent.data = intent.data!!.buildUpon()
+                                    .appendQueryParameter("q", context.getString(R.string.icon_pack)).build()
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+                            if (intent.resolveActivity(context.packageManager) != null)
+                                context.startActivity(intent)
+                            else Toast.makeText(context, R.string.no_store_found, Toast.LENGTH_LONG)
+                                    .show()
+                        },
+                startWidget = {
+                    Image(
+                            painter = painterResource(id = R.drawable.ic_add),
+                            contentDescription = "",
+                            modifier = Modifier
+                                    .size(44.dp)
+                    )
+                },
+                showDivider = true
+        )
     }
 }
