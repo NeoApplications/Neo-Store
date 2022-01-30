c9ebb5cff614b9ec27a18f062cc05cf82e742bde
Saul Henriquez <saul_henriquez@hotmail.com>
2022-01-29 18:13:02 -0600

    Apply accent to SelectTab AlertDialog
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

--- Omega/src/com/saggitt/omega/preferences/custom/MultiSelectTabPreference.kt
+++ Omega/src/com/saggitt/omega/preferences/custom/MultiSelectTabPreference.kt
@@ -21,9 +21,15 @@ package com.saggitt.omega.preferences.custom
 import android.app.Activity
 import android.content.Context
 import android.util.AttributeSet
+import androidx.appcompat.app.AlertDialog
 import androidx.preference.Preference
+import com.android.launcher3.R
 import com.android.launcher3.util.ComponentKey
 import com.saggitt.omega.groups.DrawerTabs
+import com.saggitt.omega.preferences.views.AppCategorizationFragment
+import com.saggitt.omega.preferences.views.PreferencesActivity
+import com.saggitt.omega.util.addOrRemove
+import com.saggitt.omega.util.applyColor
 import com.saggitt.omega.util.omegaPrefs
 
 class MultiSelectTabPreference(context: Context, attrs: AttributeSet?) :
@@ -38,12 +44,9 @@ class MultiSelectTabPreference(context: Context, attrs: AttributeSet?) :
 
     init {
         setOnPreferenceClickListener {
-            MultiSelectTabDialog(
-                    context,
-                    componentKey,
-                    tabs) {
+            multiSelectDialog(componentKey, tabs) {
                 callChangeListener(tabs.hashCode())
-            }.show(activity.fragmentManager, "TABS_MULTISELECT_DIALOG")
+            }
             true
         }
         setOnPreferenceChangeListener { _, _ ->
@@ -57,5 +60,54 @@ class MultiSelectTabPreference(context: Context, attrs: AttributeSet?) :
         summary = tabs
                 .filter { it.contents.value?.contains(componentKey) == true }
                 .joinToString(", ") { it.title }
+    }
+
+    private fun multiSelectDialog(componentKey: ComponentKey,
+                                  tabs: List<DrawerTabs.CustomTab>,
+                                  updatePref: () -> Unit) {
+
+        val entries = tabs.map { it.title }.toTypedArray()
+        val checkedEntries = tabs.map {
+            it.contents.value().contains(componentKey)
+        }.toBooleanArray()
+
+        val selectedItems = checkedEntries.toMutableList()
+
+        val builder = AlertDialog.Builder(context).apply {
+            setMultiChoiceItems(entries, checkedEntries) { _, which, isChecked ->
+                selectedItems[which] = isChecked
+            }
+
+            setPositiveButton(android.R.string.ok) { _, _ ->
+                tabs.forEachIndexed { index, tab ->
+                    tab.contents.value().addOrRemove(componentKey, selectedItems[index])
+                }
+                updatePref.invoke()
+            }
+
+            setNegativeButton(android.R.string.cancel) { _, _ -> }
+
+            setNeutralButton(R.string.tabs_manage) { _, _ ->
+                PreferencesActivity.startFragment(
+                        context, AppCategorizationFragment::class.java.name,
+                        context.resources.getString(R.string.title__drawer_categorization)
+                )
+            }
+        }
+
+        val alertDialog = builder.create()
+        alertDialog.setOnShowListener {
+            alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).apply {
+                applyColor(context.omegaPrefs.accentColor)
+                setTextColor(context.omegaPrefs.accentColor)
+            }
+            alertDialog.getButton(AlertDialog.BUTTON_NEUTRAL).apply {
+                setTextColor(context.omegaPrefs.accentColor)
+            }
+            alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE).apply {
+                setTextColor(context.omegaPrefs.accentColor)
+            }
+        }
+        alertDialog.show()
     }
 }
