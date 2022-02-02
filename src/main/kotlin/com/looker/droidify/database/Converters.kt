af611cb49a1ecf715335c296de5d5b2b0d720411
machiav3lli <antonios.hazim@tutanota.com>
2022-02-02 01:35:29 +0100

    Update: Release converters

--- src/main/kotlin/com/looker/droidify/database/Converters.kt
+++ src/main/kotlin/com/looker/droidify/database/Converters.kt
@@ -1,6 +1,7 @@
 package com.looker.droidify.database
 
 import androidx.room.TypeConverter
+import com.looker.droidify.entity.Release
 import com.looker.droidify.utility.jsonGenerate
 import com.looker.droidify.utility.jsonParse
 
@@ -25,4 +26,17 @@ object Converters {
     @TypeConverter
     @JvmStatic
     fun toByteArray(product: com.looker.droidify.entity.Product) = jsonGenerate(product::serialize)
+
+    @TypeConverter
+    @JvmStatic
+    fun toReleases(byteArray: ByteArray): List<Release> {
+        val string = byteArray.toString()
+        return if (string == "") emptyList()
+        else string.split(",").map { byteArray.jsonParse { Release.deserialize(it) } }
+    }
+
+    @TypeConverter
+    @JvmStatic
+    fun toByteArray(releases: List<Release>) =
+        jsonGenerate { releases.forEach { item -> item.serialize(it) }.toString().toByteArray() }
 }
