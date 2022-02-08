45cf90aebff5d63b935e39001dcf717a405cc0f2
Saul Henriquez <saul_henriquez@hotmail.com>
2022-02-08 16:10:44 -0600

    Add Option to Open SmartSpace Preference from long Click
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

--- Omega/src/com/saggitt/omega/smartspace/SmartspacePreferencesShortcut.kt
+++ Omega/src/com/saggitt/omega/smartspace/SmartspacePreferencesShortcut.kt
@@ -1,36 +1,28 @@
 package com.saggitt.omega.smartspace
 
 import android.content.Context
-import android.content.Intent
 import android.view.View
 import android.view.View.OnLongClickListener
-import com.android.launcher3.Launcher
 import com.android.launcher3.R
 import com.android.launcher3.logging.StatsLogManager.EventEnum
 import com.android.launcher3.views.OptionsPopupView.OptionItem
 import com.saggitt.omega.preferences.views.PreferencesActivity
 
-class SmartspacePreferencesShortcut(context:Context,eventId: EventEnum?) : OptionItem(
+class SmartspacePreferencesShortcut(context: Context, eventId: EventEnum?) : OptionItem(
     context.getString(R.string.customize),
     context.getDrawable(R.drawable.ic_smartspace_preferences),
     eventId,
-    OnLongClickListener { view: View -> startSmartspacePreferences(view) }) {
+    OnLongClickListener { view: View -> startSmartspacePreferences(context) }) {
 
     companion object {
-        private fun startSmartspacePreferences(view: View): Boolean {
-            val launcher = Launcher.getLauncher(view.context)
-            launcher.startActivitySafely(
-                view, Intent(launcher, PreferencesActivity::class.java)
-                    .putExtra(
-                        "title",
-                        launcher.getString(R.string.home_widget)
-                    )
-                    .putExtra(
-                        "content_res_id",
-                        R.xml.preferences_smartspace
-                    )
-                    .putExtra("has_preview", true), null
+        private fun startSmartspacePreferences(context: Context): Boolean {
+            val fragment = "com.saggitt.omega.preferences.views.PrefsWidgetFragment"
+            PreferencesActivity.startFragment(
+                context,
+                fragment,
+                context.resources.getString(R.string.home_widget)
             )
+
             return true
         }
     }
