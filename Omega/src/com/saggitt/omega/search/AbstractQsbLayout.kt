12ef1ab563cae412658bf14db7b2b76c5573449e
Saul Henriquez <saul_henriquez@hotmail.com>
2022-02-13 21:03:50 -0600

    Create ripple Drawable when clicking
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

--- Omega/src/com/saggitt/omega/search/AbstractQsbLayout.kt
+++ Omega/src/com/saggitt/omega/search/AbstractQsbLayout.kt
@@ -21,26 +21,50 @@ package com.saggitt.omega.search
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.drawable.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.content.ContextCompat
 import com.android.launcher3.R
+import com.android.launcher3.ResourceUtils
 import com.android.launcher3.Utilities
+import com.android.launcher3.graphics.IconShape
 import com.saggitt.omega.preferences.OmegaPreferences
 import com.saggitt.omega.util.Config
+import kotlin.math.round
+
 
 abstract class AbstractQsbLayout(context: Context, attrs: AttributeSet? = null) :
-    FrameLayout(context, attrs) {
+    FrameLayout(context, attrs), SharedPreferences.OnSharedPreferenceChangeListener {
     var mContext: Context = context
     protected var prefs: OmegaPreferences = Utilities.getOmegaPrefs(mContext)
     protected var controller = SearchProviderController.getInstance(getContext())
     protected var searchProvider: SearchProvider = controller.searchProvider
+    private var mRadius = -1.0f
+    private var mShowAssistant = false
+    protected var mIsRtl = Utilities.isRtl(resources)
 
     private var micIconView: ImageView? = null
     private var searchLogoView: ImageView? = null
-    private var lensIcon: ImageView? = null
+    private var lensIconView: ImageView? = null
+    private var mShadowMargin = 0
+
+    var micStrokeWidth = 0f
+    protected var mMicStrokePaint: Paint? = null
+
+    init {
+        mShadowMargin = resources.getDimensionPixelSize(R.dimen.qsb_shadow_margin);
+        mMicStrokePaint = Paint(1);
+        mMicStrokePaint?.setColor(Color.WHITE);
+    }
 
     override fun onFinishInflate() {
         super.onFinishInflate()
@@ -82,7 +106,7 @@ abstract class AbstractQsbLayout(context: Context, attrs: AttributeSet? = null) 
             }
         }
 
-        lensIcon = findViewById<ImageView?>(R.id.lens_icon).apply {
+        lensIconView = findViewById<ImageView?>(R.id.lens_icon).apply {
             val lensIntent = Intent.makeMainActivity(
                 ComponentName(
                     Config.LENS_PACKAGE,
@@ -117,6 +141,123 @@ abstract class AbstractQsbLayout(context: Context, attrs: AttributeSet? = null) 
                 ).setPackage(searchProvider.packageName)
             )
         }
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        getDevicePreferences().registerOnSharedPreferenceChangeListener(this)
+
+        addOrUpdateSearchRipple()
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        getDevicePreferences().unregisterOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String?) {
+        when (key) {
+            "opa_enabled",
+            "opa_assistant",
+            "pref_searchbarRadius" ->
+                reloadPreferences(sharedPreferences)
+        }
+    }
+
+    /*
+    * Create the search background when clicked
+    * */
+    private fun addOrUpdateSearchRipple() {
+        val insetDrawable: InsetDrawable = createRipple().mutate() as InsetDrawable
+        background = insetDrawable
+
+        val oldRipple: RippleDrawable = insetDrawable.drawable as RippleDrawable
+        oldRipple.setLayerInset(0, 0, 0, 0, 0)
+
+        searchLogoView?.apply {
+            val searchIconRipple: RippleDrawable =
+                oldRipple.constantState!!.newDrawable().mutate() as RippleDrawable
+            searchIconRipple.setLayerInset(0, 0, 2, 0, 2);
+            background = searchIconRipple
+            setPadding(8, 8, 8, 8)
+        }
+
+        micIconView?.apply {
+            val micIconRipple: RippleDrawable =
+                oldRipple.constantState!!.newDrawable().mutate() as RippleDrawable
+            micIconRipple.setLayerInset(0, 0, 2, 0, 2);
+            background = micIconRipple
+            setPadding(12, 12, 12, 12)
+        }
+
+        lensIconView?.apply {
+            val lensIconRipple: RippleDrawable =
+                oldRipple.constantState!!.newDrawable().mutate() as RippleDrawable
+            lensIconRipple.setLayerInset(0, 0, 2, 0, 2);
+            background = lensIconRipple
+            setPadding(12, 12, 12, 12)
+        }
+        lensIconView?.requestLayout()
+    }
+
+    private fun createRipple(): InsetDrawable {
+        val shape = GradientDrawable()
+        shape.shape = GradientDrawable.RECTANGLE
+        shape.cornerRadius = getCornerRadius()
+        shape.setColor(ContextCompat.getColor(context, R.color.colorAccent))
+        val rippleColor: ColorStateList =
+            ContextCompat.getColorStateList(context, R.color.focused_background)!!
+        val ripple = RippleDrawable(rippleColor, null, shape)
+        return InsetDrawable(ripple, resources.getDimensionPixelSize(R.dimen.qsb_shadow_margin))
+    }
+
+    protected fun getDevicePreferences(): SharedPreferences {
+        val devicePrefs = Utilities.getPrefs(context)
+        reloadPreferences(devicePrefs)
+        return devicePrefs
+    }
+
+    private fun reloadPreferences(sharedPreferences: SharedPreferences) {
+        post {
+            searchProvider = SearchProviderController.getInstance(mContext).searchProvider
+            mShowAssistant = sharedPreferences.getBoolean("opa_assistant", true)
+            searchLogoView?.setImageDrawable(getIcon())
+            micIconView?.visibility = View.VISIBLE
+            micIconView?.setImageDrawable(getMicIcon())
+            mRadius = getCornerRadius()
+
+            invalidate()
+        }
+    }
+
+    protected open fun getCornerRadius(): Float {
+        val defaultRadius = ResourceUtils.pxFromDp(100f, resources.displayMetrics).toFloat()
+        val radius: Float = round(Utilities.getOmegaPrefs(context).searchBarRadius)
+        if (radius >= 0f) {
+            return radius
+        }
+        val edgeRadius: TypedValue? = IconShape.getShape().getAttrValue(R.attr.qsbEdgeRadius)
+        return edgeRadius?.getDimension(context.resources.displayMetrics) ?: defaultRadius
+    }
+
+    private fun getMicIcon(): Drawable? {
+        return if (prefs.allAppsGlobalSearch) {
+            if (searchProvider.supportsAssistant && mShowAssistant) {
+                searchProvider.getAssistantIcon(true)
+            } else if (searchProvider.supportsVoiceSearch) {
+                searchProvider.getVoiceIcon(true)
+            } else {
+                micIconView?.visibility = GONE
+                ColorDrawable(Color.TRANSPARENT)
+            }
+        } else {
+            micIconView?.visibility = GONE
+            ColorDrawable(Color.TRANSPARENT)
+        }
+    }
+
+    protected fun getIcon(): Drawable {
+        return searchProvider.getIcon(true)
     }
 
 }
