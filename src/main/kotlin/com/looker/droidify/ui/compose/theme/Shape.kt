0733b80c56d2d1e7527f41a719647c79bd692afa
machiav3lli <antonios.hazim@tutanota.com>
2022-02-17 23:11:34 +0100

    Add: Duplicate xml shapes themes to compose ones

---
+++ src/main/kotlin/com/looker/droidify/ui/compose/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.looker.droidify.ui.compose.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val AppShapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(8.dp),
+    large = RoundedCornerShape(16.dp)
+)
