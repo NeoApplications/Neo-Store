0935402c5e3fe9b5a6bb493a947866244491593d
machiav3lli <antonios.hazim@tutanota.com>
2022-02-17 23:10:55 +0100

    Add: Duplicate xml colors to compose ones

---
+++ src/main/kotlin/com/looker/droidify/ui/compose/theme/Color.kt
@@ -0,0 +1,57 @@
+package com.looker.droidify.ui.compose.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PitchBlack = Color(0xFF000000)
+val GreyDark = Color(0xFF2C2C2C)
+
+// Light Theme
+val LightPrimary = Color(0xFF006D3E)
+val LightOnPrimary = Color(0xFFFFFFFF)
+val LightPrimaryContainer = Color(0xFF71FCAD)
+val LightOnPrimaryContainer = Color(0xFF00210F)
+val LightSecondary = Color(0xFF924C00)
+val LightOnSecondary = Color(0xFFFFFFFF)
+val LightSecondaryContainer = Color(0xFFFFDCC1)
+val LightOnSecondaryContainer = Color(0xFF2F1400)
+val LightError = Color(0xFFBA1B1B)
+val LightErrorContainer = Color(0xFFFFDAD4)
+val LightOnError = Color(0xFFFFFFFF)
+val LightOnErrorContainer = Color(0xFF410001)
+val LightBackground = Color(0xFFFBFDF8)
+val LightOnBackground = Color(0xFF191C1A)
+val LightSurface = Color(0xFFE8F5E9)
+val LightOnSurface = Color(0xFF191C1A)
+val LightSurfaceVariant = Color(0xFFDDE5DC)
+val LightOnSurfaceVariant = Color(0xFF414942)
+val LightOutline = Color(0xFF707971)
+val LightInversePrimary = Color(0xFF51DF93)
+val LightInverseSurface = Color(0xFF2E312E)
+val LightInverseOnSurface = Color(0xFFEFF1EC)
+
+// Dark Theme
+val DarkPrimary = Color(0xFF51DF93)
+val DarkOnPrimary = Color(0xFF00391D)
+val DarkPrimaryContainer = Color(0xFF00522D)
+val DarkOnPrimaryContainer = Color(0xFF71FCAD)
+val DarkSecondary = Color(0xFFFFB77B)
+val DarkOnSecondary = Color(0xFF4E2600)
+val DarkSecondaryContainer = Color(0xFF6F3800)
+val DarkOnSecondaryContainer = Color(0xFFFFDCC1)
+val DarkError = Color(0xFFFFB4A9)
+val DarkErrorContainer = Color(0xFF930006)
+val DarkOnError = Color(0xFF680003)
+val DarkOnErrorContainer = Color(0xFFFFDAD4)
+val DarkBackground = Color(0xFF191C1A)
+val DarkOnBackground = Color(0xFFE1E3DE)
+val DarkSurface = Color(0xFF191C1A)
+val DarkOnSurface = Color(0xFFE1E3DE)
+val DarkSurfaceVariant = Color(0xFF414942)
+val DarkOnSurfaceVariant = Color(0xFFC0C9C0)
+val DarkOutline = Color(0xFF8A938A)
+val DarkInversePrimary = Color(0xFFE1E3DE)
+val DarkInverseSurface = Color(0xFF191C1A)
+val DarkInverseOnSurface = Color(0xFF006D3E)
+
+val Seed = Color(0xFF51DF93)
+
