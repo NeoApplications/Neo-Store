f9902597fd589ecc0a70ec4601ee7c19ed071bbe
machiav3lli <antonios.hazim@tutanota.com>
2022-02-27 00:06:48 +0100

    Add: Product Items' Previews

--- src/main/kotlin/com/looker/droidify/ui/compose/components/ProductCard.kt
+++ src/main/kotlin/com/looker/droidify/ui/compose/components/ProductCard.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.looker.droidify.database.entity.Repository
 import com.looker.droidify.entity.ProductItem
@@ -71,4 +72,10 @@ fun ProductCard(
             color = MaterialTheme.colorScheme.onSurfaceVariant
         )
     }
+}
+
+@Preview
+@Composable
+fun ProductCardPreview() {
+    ProductCard(ProductItem())
 }
--- src/main/kotlin/com/looker/droidify/ui/compose/components/ProductsListItem.kt
+++ src/main/kotlin/com/looker/droidify/ui/compose/components/ProductsListItem.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.looker.droidify.R
 import com.looker.droidify.database.entity.Repository
@@ -127,4 +128,10 @@ fun ExpandedItemContent(
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun ProductsListItemPreview() {
+    ProductsListItem(ProductItem())
 }
