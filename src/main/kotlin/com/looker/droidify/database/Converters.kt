9a5741ed3b890cdb377cce99bb08672f12c9f8be
machiav3lli <antonios.hazim@tutanota.com>
2022-04-06 04:24:28 +0200

    Fix: Parsing index when there's ; in the serialized values

--- src/main/kotlin/com/looker/droidify/database/Converters.kt
+++ src/main/kotlin/com/looker/droidify/database/Converters.kt
@@ -41,52 +41,52 @@ object Converters {
     @JvmStatic
     fun toReleases(byteArray: ByteArray): List<Release> =
         if (String(byteArray) == "") emptyList()
-        else String(byteArray).split(";").map { Release.fromJson(it) }
+        else String(byteArray).split("|").map { Release.fromJson(it) }
 
     @JvmName("releasesToByteArray")
     @TypeConverter
     @JvmStatic
     fun toByteArray(releases: List<Release>) =
-        if (releases.isNotEmpty()) releases.joinToString(";") { it.toJSON() }.toByteArray()
+        if (releases.isNotEmpty()) releases.joinToString("|") { it.toJSON() }.toByteArray()
         else "".toByteArray()
 
     @TypeConverter
     @JvmStatic
     fun toIncompatibilities(byteArray: ByteArray): List<Release.Incompatibility> =
         if (String(byteArray) == "") emptyList()
-        else String(byteArray).split(";").map { Release.Incompatibility.fromJson(it) }
+        else String(byteArray).split("|").map { Release.Incompatibility.fromJson(it) }
 
     @JvmName("incompatibilitiesToByteArray")
     @TypeConverter
     @JvmStatic
     fun toByteArray(incompatibilities: List<Release.Incompatibility>) =
         if (incompatibilities.isNotEmpty())
-            incompatibilities.joinToString(";") { it.toJSON() }.toByteArray()
+            incompatibilities.joinToString("|") { it.toJSON() }.toByteArray()
         else "".toByteArray()
 
     @TypeConverter
     @JvmStatic
     fun toScreenshots(byteArray: ByteArray): List<Screenshot> =
         if (String(byteArray) == "") emptyList()
-        else String(byteArray).split(";").map { Screenshot.fromJson(it) }
+        else String(byteArray).split("|").map { Screenshot.fromJson(it) }
 
     @JvmName("screenshotsToByteArray")
     @TypeConverter
     @JvmStatic
     fun toByteArray(screenshots: List<Screenshot>) =
-        if (screenshots.isNotEmpty()) screenshots.joinToString(";") { it.toJSON() }.toByteArray()
+        if (screenshots.isNotEmpty()) screenshots.joinToString("|") { it.toJSON() }.toByteArray()
         else "".toByteArray()
 
     @TypeConverter
     @JvmStatic
     fun toDonates(byteArray: ByteArray): List<Donate> =
         if (String(byteArray) == "") emptyList()
-        else String(byteArray).split(";").map { Donate.fromJson(it) }
+        else String(byteArray).split("|").map { Donate.fromJson(it) }
 
     @JvmName("donatesToByteArray")
     @TypeConverter
     @JvmStatic
     fun toByteArray(donates: List<Donate>) =
-        if (donates.isNotEmpty()) donates.joinToString(";") { it.toJSON() }.toByteArray()
+        if (donates.isNotEmpty()) donates.joinToString("|") { it.toJSON() }.toByteArray()
         else "".toByteArray()
 }
