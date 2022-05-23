93144b0e4f37437dee94cfcb9ffa8bcf693ef229
Saul <saul_henriquez@hotmail.com>
2022-05-22 20:17:03 -0500

    Improve ListItemWithIcon UI

--- Omega/src/com/saggitt/omega/compose/components/ListItemWithIcon.kt
+++ Omega/src/com/saggitt/omega/compose/components/ListItemWithIcon.kt
@@ -23,13 +23,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Divider
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.LocalContentColor
-import androidx.compose.material3.LocalTextStyle
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RadioButton
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
@@ -89,7 +83,7 @@ fun ListItemWithIcon(
             ) {
                 CompositionLocalProvider(
                     LocalContentColor provides MaterialTheme.colorScheme.onBackground,
-                    LocalTextStyle provides MaterialTheme.typography.titleLarge
+                    LocalTextStyle provides MaterialTheme.typography.titleMedium
                 ) {
                     title()
                 }
