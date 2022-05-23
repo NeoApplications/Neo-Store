f80f15cba03bf0a09675594933ae7fe1490671dc
Saul <saul_henriquez@hotmail.com>
2022-05-23 13:54:40 -0500

    Create compose SwitchPreference

---
+++ Omega/src/com/saggitt/omega/compose/components/SwitchPreference.kt
@@ -0,0 +1,124 @@
+/*
+ * This file is part of Neo Launcher
+ * Copyright (c) 2022   Neo Launcher Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.saggitt.omega.compose.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.saggitt.omega.util.addIf
+
+@Composable
+fun SwitchPreference(
+    title: String,
+    modifier: Modifier = Modifier,
+    summary: String = "",
+    startIcon: (@Composable () -> Unit)? = null,
+    onCheckedChange: ((Boolean) -> Unit)? = null,
+    isChecked: Boolean = false,
+    isEnabled: Boolean = true,
+    showDivider: Boolean = false,
+    dividerIndent: Dp = 0.dp,
+    applyPaddings: Boolean = true,
+    horizontalPadding: Dp = 16.dp,
+    verticalPadding: Dp = 16.dp,
+    verticalAlignment: Alignment.Vertical = Alignment.CenterVertically
+) {
+    val checkedState = remember { mutableStateOf(isChecked) }
+    Column(
+        modifier = Modifier.clickable(enabled = isEnabled) {
+            checkedState.value = !checkedState.value
+            onCheckedChange?.invoke(checkedState.value)
+        }
+    ) {
+        if (showDivider) {
+            Divider(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                startIndent = dividerIndent,
+            )
+        }
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .addIf(applyPaddings) {
+                    padding(horizontal = horizontalPadding, vertical = verticalPadding)
+                },
+            verticalAlignment = verticalAlignment,
+        ) {
+            startIcon?.let {
+                startIcon()
+                if (applyPaddings) {
+                    Spacer(modifier = Modifier.requiredWidth(16.dp))
+                }
+            }
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .addIf(!isEnabled) {
+                        alpha(0.3f)
+                    }
+            ) {
+                Text(
+                    text = title,
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    style = MaterialTheme.typography.titleMedium,
+                    fontSize = 16.sp
+                )
+                if (summary != "") {
+                    Text(
+                        text = summary,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        style = MaterialTheme.typography.bodyMedium,
+                    )
+                }
+            }
+            Switch(
+                modifier = Modifier
+                    .height(24.dp),
+                checked = checkedState.value,
+                onCheckedChange = onCheckedChange?.let {
+                    { checkedState.value = it }
+                },
+                enabled = isEnabled,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SwitchPreferencePreview() {
+    SwitchPreference(
+        title = "Ocultar de Drawer",
+        summary = "Oculta el icono del menu principal"
+    )
+}
