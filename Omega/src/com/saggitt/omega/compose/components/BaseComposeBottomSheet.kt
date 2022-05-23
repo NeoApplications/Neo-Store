d14e398dd3cdbee79568f55949bc9d931d438213
Saul <saul_henriquez@hotmail.com>
2022-05-23 06:43:32 -0500

    Fix compose bottom sheet nav padding

--- Omega/src/com/saggitt/omega/compose/components/BaseComposeBottomSheet.kt
+++ Omega/src/com/saggitt/omega/compose/components/BaseComposeBottomSheet.kt
@@ -8,11 +8,13 @@ import android.widget.LinearLayout
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import com.android.launcher3.Launcher
 import com.android.launcher3.R
 import com.android.launcher3.anim.Interpolators
@@ -48,6 +50,7 @@ class BaseComposeBottomSheet(context: Context) : AbstractSlideInView<Launcher>(c
                             MaterialTheme.colorScheme.background,
                             MaterialTheme.shapes.large
                         )
+                        .padding(bottom = 50.dp)
                         .wrapContentHeight()
                         .fillMaxWidth()
                 ) {
