ebf4d1c9379b87e371f23a198cc814a7ba77f24a
Saul <saul_henriquez@hotmail.com>
2022-05-25 11:00:20 -0500

    Add PreferencesGraph

---
+++ Omega/src/com/saggitt/omega/compose/preferences/PreferencesGraph.kt
@@ -0,0 +1,28 @@
+package com.saggitt.omega.compose.preferences
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
+import androidx.navigation.NavGraphBuilder
+import com.google.accompanist.navigation.animation.composable
+
+@OptIn(ExperimentalAnimationApi::class)
+inline fun NavGraphBuilder.preferenceGraph(
+    route: String,
+    crossinline root: @Composable () -> Unit,
+    crossinline block: NavGraphBuilder.(subRoute: (String) -> String) -> Unit = { }
+) {
+    val subRoute: (String) -> String = { name -> "$route$name/" }
+    composable(route = route) {
+        CompositionLocalProvider(LocalRoute provides route) {
+            root()
+        }
+    }
+    block(subRoute)
+}
+
+val LocalRoute = compositionLocalOf { "" }
+
+@Composable
+fun subRoute(name: String) = "${LocalRoute.current}$name/"
