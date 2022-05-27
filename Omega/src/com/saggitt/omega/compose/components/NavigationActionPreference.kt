b67f73c8af662a5b1a99f09ad9421de20f6f09cc
Saul <saul_henriquez@hotmail.com>
2022-05-27 14:50:05 -0500

    Add tint color to the Icon

--- Omega/src/com/saggitt/omega/compose/components/NavigationActionPreference.kt
+++ Omega/src/com/saggitt/omega/compose/components/NavigationActionPreference.kt
@@ -1,11 +1,11 @@
 package com.saggitt.omega.compose.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -51,9 +51,10 @@ fun NavigationActionPreference(
                 .fillMaxWidth()
                 .padding(horizontal = horizontalPadding, vertical = verticalPadding)
         ) {
-            Image(
+            Icon(
                 painter = painterResource(id = icon),
                 contentDescription = null,
+                tint = MaterialTheme.colorScheme.primary,
                 modifier = Modifier
                     .clip(CircleShape)
                     .size(32.dp)
