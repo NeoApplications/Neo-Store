ca679712811a535470011a150ed680cbc4de5b62
Saul <saul_henriquez@hotmail.com>
2022-05-27 15:27:43 -0500

    Add ComposeViewWithActionBar

---
+++ Omega/src/com/saggitt/omega/compose/components/ViewWithActionBar.kt
@@ -0,0 +1,65 @@
+/*
+ * This file is part of Neo Launcher
+ * Copyright (c) 2022   Neo Launcher Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.saggitt.omega.compose.components
+
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.android.launcher3.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ViewWithActionBar(
+    title: String,
+    content: @Composable (PaddingValues) -> Unit,
+) {
+    val backDispatcher = LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = title, style = MaterialTheme.typography.titleMedium)
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = { backDispatcher?.onBackPressed() }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.gesture_press_back),
+                        )
+                    }
+                },
+                modifier = Modifier.padding(top = 26.dp),
+                backgroundColor = MaterialTheme.colorScheme.background,
+                elevation = 0.dp
+            )
+        },
+        content = content
+    )
+}
