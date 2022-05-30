80003636cb3ca413b4c00e9bc6b828782d89792c
Saul <saul_henriquez@hotmail.com>
2022-05-30 13:01:55 -0500

    prevent keyboard from hiding app name Textfield

--- Omega/src/com/saggitt/omega/compose/components/ComposeBottomSheet.kt
+++ Omega/src/com/saggitt/omega/compose/components/ComposeBottomSheet.kt
@@ -3,22 +3,26 @@ package com.saggitt.omega.compose.components
 import android.animation.PropertyValuesHolder
 import android.content.Context
 import android.util.AttributeSet
+import android.util.FloatProperty
 import android.view.Gravity
 import android.view.ViewGroup
+import android.view.animation.Interpolator
 import android.widget.LinearLayout
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import com.android.launcher3.Launcher
 import com.android.launcher3.R
+import com.android.launcher3.Utilities
 import com.android.launcher3.anim.Interpolators
+import com.android.launcher3.anim.PendingAnimation
 import com.android.launcher3.util.SystemUiController
 import com.android.launcher3.util.Themes
 import com.android.launcher3.views.AbstractSlideInView
@@ -31,6 +35,11 @@ class ComposeBottomSheet(context: Context, attrs: AttributeSet? = null) :
 
     private val container = ComposeView(context)
     private val mLauncher = Launcher.getLauncher(context)
+    private var imeShift = 0f
+    private var _hintCloseProgress = mutableStateOf(0f)
+    private val hintCloseProgress get() = _hintCloseProgress.value
+    var hintCloseDistance = 0f
+        private set
 
     init {
         layoutParams =
@@ -44,10 +53,18 @@ class ComposeBottomSheet(context: Context, attrs: AttributeSet? = null) :
         }
     }
 
-    fun setContent(content: @Composable ComposeBottomSheet.() -> Unit) {
+    fun setContent(
+        content: @Composable ComposeBottomSheet.() -> Unit,
+        contentPaddings: PaddingValues = PaddingValues(
+            top = 0.dp,
+            bottom = 50.dp,
+            start = 0.dp,
+            end = 0.dp
+        )
+    ) {
         container.setContent {
             OmegaAppTheme {
-                Box(
+                /*Box(
                     modifier = Modifier
                         .background(
                             MaterialTheme.colorScheme.background,
@@ -58,6 +75,9 @@ class ComposeBottomSheet(context: Context, attrs: AttributeSet? = null) :
                         .fillMaxWidth()
                 ) {
                     content(this@ComposeBottomSheet)
+                }*/
+                ContentWrapper(contentPaddings) {
+                    content(this)
                 }
             }
         }
@@ -102,12 +122,74 @@ class ComposeBottomSheet(context: Context, attrs: AttributeSet? = null) :
         return type and TYPE_SETTINGS_SHEET != 0
     }
 
+    private fun setImeShift(shift: Float) {
+        imeShift = shift
+        updateContentShift()
+    }
+
+    override fun addHintCloseAnim(
+        distanceToMove: Float,
+        interpolator: Interpolator,
+        target: PendingAnimation
+    ) {
+        super.addHintCloseAnim(distanceToMove, interpolator, target)
+        hintCloseDistance = distanceToMove
+        target.setFloat(this, HINT_CLOSE_PROGRESS, 1f, interpolator)
+    }
+
+    private fun updateContentShift() {
+        if (!Utilities.getOmegaPrefs(context).showDebugInfo) {
+            mContent.translationY = mTranslationShift * mContent.height + imeShift
+        }
+    }
+
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
         super.onLayout(changed, l, t, r, b)
         setTranslationShift(mTranslationShift)
     }
 
+    @Composable
+    private fun ContentWrapper(
+        contentPaddings: PaddingValues = PaddingValues(all = 0.dp),
+        content: @Composable ComposeBottomSheet.() -> Unit
+    ) {
+        val imePaddings = WindowInsets.ime
+            .only(WindowInsetsSides.Horizontal + WindowInsetsSides.Bottom)
+            .asPaddingValues()
+
+        val translation = imePaddings - contentPaddings
+        setImeShift(with(LocalDensity.current) { -translation.calculateBottomPadding().toPx() })
+
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth(),
+            shape = MaterialTheme.shapes.large,
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Box(
+                modifier = Modifier
+                    .padding(contentPaddings)
+                    .graphicsLayer(
+                        alpha = 1f - (hintCloseProgress * 0.5f),
+                        translationY = hintCloseProgress * -hintCloseDistance
+                    )
+            ) {
+                content(this@ComposeBottomSheet)
+            }
+        }
+    }
+
     companion object {
+
+        private val HINT_CLOSE_PROGRESS =
+            object : FloatProperty<ComposeBottomSheet>("hintCloseProgress") {
+                override fun setValue(view: ComposeBottomSheet, value: Float) {
+                    view._hintCloseProgress.value = value
+                }
+
+                override fun get(view: ComposeBottomSheet) = view._hintCloseProgress.value
+            }
+
         fun show(
             context: Context,
             animate: Boolean = true,
--- Omega/src/com/saggitt/omega/compose/components/PaddingValues.kt
+++ Omega/src/com/saggitt/omega/compose/components/PaddingValues.kt
@@ -39,3 +39,35 @@ private class CombinePaddingValues(private val a: PaddingValues, private val b: 
     override fun calculateBottomPadding() =
         a.calculateBottomPadding() + b.calculateBottomPadding()
 }
+
+@Composable
+operator fun PaddingValues.minus(b: PaddingValues): PaddingValues {
+    val a = this
+    return remember(a, b) {
+        object : PaddingValues {
+            override fun calculateLeftPadding(layoutDirection: LayoutDirection): Dp {
+                val aLeft = a.calculateLeftPadding(layoutDirection)
+                val bLeft = b.calculateRightPadding(layoutDirection)
+                return (aLeft - bLeft).coerceAtLeast(0.dp)
+            }
+
+            override fun calculateTopPadding(): Dp {
+                val aTop = a.calculateTopPadding()
+                val bTop = b.calculateTopPadding()
+                return (aTop - bTop).coerceAtLeast(0.dp)
+            }
+
+            override fun calculateRightPadding(layoutDirection: LayoutDirection): Dp {
+                val aRight = a.calculateRightPadding(layoutDirection)
+                val bRight = b.calculateRightPadding(layoutDirection)
+                return (aRight - bRight).coerceAtLeast(0.dp)
+            }
+
+            override fun calculateBottomPadding(): Dp {
+                val aBottom = a.calculateBottomPadding()
+                val bBottom = b.calculateBottomPadding()
+                return (aBottom - bBottom).coerceAtLeast(0.dp)
+            }
+        }
+    }
+}
