227dd9b15ed58a23ed8a0eacb7d57d5e712840f1
Saul <saul_henriquez@hotmail.com>
2022-06-05 16:56:09 -0500

    Improve ComposeActivity view

--- Omega/src/com/saggitt/omega/compose/ComposeActivity.kt
+++ Omega/src/com/saggitt/omega/compose/ComposeActivity.kt
@@ -26,24 +26,56 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.core.net.toUri
-import androidx.core.view.WindowCompat
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.saggitt.omega.compose.navigation.DefaultComposeView
 import com.saggitt.omega.theme.OmegaAppTheme
+import com.saggitt.omega.theme.ThemeManager
+import com.saggitt.omega.theme.ThemeOverride
+import com.saggitt.omega.util.omegaPrefs
+import com.saggitt.omega.util.recreateAnimated
 
 /*
     Blank activity to handle Compose calls
  */
-class ComposeActivity : AppCompatActivity() {
+class ComposeActivity : AppCompatActivity(), ThemeManager.ThemeableActivity {
+
+    override var currentTheme = 0
+    override var currentAccent = 0
+    private lateinit var themeOverride: ThemeOverride
+    private val themeSet: ThemeOverride.ThemeSet get() = ThemeOverride.Settings()
+    private var paused = false
+
     @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        themeOverride = ThemeOverride(themeSet, this)
+        themeOverride.applyTheme(this)
+        currentAccent = omegaPrefs.accentColor
+        currentTheme = themeOverride.getTheme(this)
+        theme.applyStyle(
+            resources.getIdentifier(
+                Integer.toHexString(currentAccent),
+                "style",
+                packageName
+            ), true
+        )
+        if (supportActionBar != null) {
+            supportActionBar?.hide()
+        }
         setContent {
             OmegaAppTheme {
                 val navController = rememberAnimatedNavController()
                 DefaultComposeView(navController)
             }
+        }
+    }
+
+    override fun onThemeChanged() {
+        if (currentTheme == themeOverride.getTheme(this)) return
+        if (paused) {
+            recreate()
+        } else {
+            recreateAnimated()
         }
     }
 
