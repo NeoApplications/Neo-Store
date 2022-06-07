1b92ef93bf88d84010d9ee1d0b8b9f711898b2b2
Iamlooker <mohit2002ss@gmail.com>
2022-06-07 15:21:52 +0530

    Cleanup

--- src/main/kotlin/com/looker/droidify/ui/compose/components/ActionButton.kt
+++ src/main/kotlin/com/looker/droidify/ui/compose/components/ActionButton.kt
@@ -1,24 +1,8 @@
 package com.looker.droidify.ui.compose.components
 
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.SizeTransform
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
-import androidx.compose.animation.with
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.defaultMinSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.animation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -53,9 +37,6 @@ fun MainActionButton(
             },
         )
     ) {
-        val content = @Composable {
-        }
-
         AnimatedContent(
             targetState = actionState,
             transitionSpec = {
@@ -85,7 +66,6 @@ fun MainActionButton(
     }
 }
 
-@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun SecondaryActionButton(
     modifier: Modifier = Modifier,
