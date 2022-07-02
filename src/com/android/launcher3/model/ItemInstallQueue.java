a9a90e506c439d1d7abb58433e13d8d99b3e2ee8
Saul Henriquez <saul_henriquez@hotmail.com>
2022-07-01 19:04:17 -0500

    Launcher3: Do not attempt to show promise icon when info is null
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

--- src/com/android/launcher3/model/ItemInstallQueue.java
+++ src/com/android/launcher3/model/ItemInstallQueue.java
@@ -105,6 +105,7 @@ public class ItemInstallQueue {
 
     @WorkerThread
     private void addToQueue(PendingInstallShortcutInfo info) {
+        if (info == null) return;
         ensureQueueLoaded();
         if (!mItems.contains(info)) {
             mItems.add(info);
