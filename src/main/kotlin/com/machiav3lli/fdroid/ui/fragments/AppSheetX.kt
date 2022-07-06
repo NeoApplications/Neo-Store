12531219381e6676d2c2bd0e4ff11dac8c5c010d
machiav3lli <antonios.hazim@tutanota.com>
2022-07-07 00:38:53 +0200

    Add: Releases' section title

--- src/main/kotlin/com/machiav3lli/fdroid/ui/fragments/AppSheetX.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/fragments/AppSheetX.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -607,7 +608,13 @@ class AppSheetX() : FullscreenBottomSheetDialogFragment(), Callbacks {
                             }
                         }
                     }
-
+                    item {
+                        Text(
+                            text = stringResource(id = R.string.releases),
+                            style = MaterialTheme.typography.titleMedium,
+                            modifier = Modifier.padding(start = 14.dp)
+                        )
+                    }
                     items(items = releaseItems) { item ->
                         ReleaseItem(
                             release = item.first,
