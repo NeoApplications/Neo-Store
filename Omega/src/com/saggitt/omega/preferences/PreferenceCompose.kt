fb550011cd22a98ed4c427d2fa9a76e3f948961c
machiav3lli <antonios.hazim@tutanota.com>
2022-07-30 03:07:07 +0200

    Add: Base-, Boolean and Seekbar Compose Prefs

---
+++ Omega/src/com/saggitt/omega/preferences/PreferenceCompose.kt
@@ -0,0 +1,160 @@
+package com.saggitt.omega.preferences
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.saggitt.omega.util.addIf
+
+@Composable
+fun BasePreference(
+    modifier: Modifier = Modifier,
+    @StringRes titleId: Int,
+    @StringRes summaryId: Int = -1,
+    summary: String? = null,
+    isEnabled: Boolean = true,
+    endWidget: (@Composable () -> Unit)? = null,
+    bottomWidget: (@Composable () -> Unit)? = null,
+    onClick: (() -> Unit)? = null,
+) {
+    Column(
+        modifier = Modifier
+            .clip(MaterialTheme.shapes.medium)
+            .addIf(onClick != null) {
+                clickable(enabled = isEnabled, onClick = onClick!!)
+            }
+    ) {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp, vertical = 12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .addIf(!isEnabled) {
+                        alpha(0.3f)
+                    }
+            ) {
+                Text(
+                    text = stringResource(id = titleId),
+                    color = MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.titleMedium,
+                    fontSize = 16.sp
+                )
+                if (summaryId != -1 || summary != null) {
+                    Text(
+                        text = summary ?: stringResource(id = summaryId),
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        style = MaterialTheme.typography.bodyMedium,
+                    )
+                }
+                bottomWidget?.let {
+                    Spacer(modifier = Modifier.requiredWidth(8.dp))
+                    bottomWidget()
+                }
+            }
+            endWidget?.let {
+                Spacer(modifier = Modifier.requiredWidth(8.dp))
+                endWidget()
+            }
+        }
+    }
+}
+
+@Composable
+fun SwitchPreference(
+    modifier: Modifier = Modifier,
+    pref: BasePreferences.BooleanPref,
+    isEnabled: Boolean = true,
+    onCheckedChange: ((Boolean) -> Unit) = {},
+) {
+    val context = LocalContext.current
+    val (checked, check) = remember(pref) { mutableStateOf(pref.onGetValue()) }
+
+    BasePreference(
+        modifier = modifier,
+        titleId = pref.titleId,
+        summaryId = pref.summaryId,
+        isEnabled = isEnabled,
+        onClick = {
+            onCheckedChange(!checked)
+            check(!checked)
+        },
+        endWidget = {
+            Switch(
+                modifier = Modifier
+                    .height(24.dp),
+                checked = checked,
+                onCheckedChange = {
+                    onCheckedChange(it)
+                    check(it)
+                },
+                enabled = isEnabled,
+            )
+        }
+    )
+}
+
+@Composable
+fun SeekBarPreference(
+    modifier: Modifier = Modifier,
+    pref: BasePreferences.FloatPref,
+    isEnabled: Boolean = true,
+    onValueChange: ((Float) -> Unit) = {},
+) {
+    val (currentValue, setCurrentValue) = remember(pref) { mutableStateOf(pref.onGetValue()) }
+
+    BasePreference(
+        modifier = modifier,
+        titleId = pref.titleId,
+        summaryId = pref.summaryId,
+        isEnabled = isEnabled,
+        bottomWidget = {
+            Row {
+                Text(
+                    text = pref.specialOutputs(currentValue),
+                    modifier = Modifier.widthIn(min = 72.dp)
+                )
+                Spacer(modifier = Modifier.requiredWidth(8.dp))
+                Slider(
+                    modifier = Modifier
+                        .requiredHeight(24.dp)
+                        .weight(1f),
+                    value = currentValue,
+                    valueRange = pref.minValue..pref.maxValue,
+                    onValueChange = { setCurrentValue(it) },
+                    onValueChangeFinished = {
+                        pref.onSetValue(currentValue)
+                        pref.onChange()
+                        onValueChange(currentValue)
+                    },
+                    enabled = isEnabled
+                )
+            }
+        }
+    )
+}
