881af210e4e295af54b7c458dc2d1026b39265d0
machiav3lli <antonios.hazim@tutanota.com>
2022-08-06 14:02:40 +0200

    0.9.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,7 +78,6 @@ android {
             isMinifyEnabled = false
             isShrinkResources = false
             applicationIdSuffix = ".neo"
-            versionNameSuffix = "-rc1"
             resValue("string", "application_name", "Neo Store-beta")
             manifestPlaceholders["appIcon"] = "@mipmap/ic_launcher_debug"
             manifestPlaceholders["appIconRound"] = "@mipmap/ic_launcher_round_debug"
@@ -86,7 +85,6 @@ android {
         named("release") {
             isMinifyEnabled = true
             isShrinkResources = true
-            versionNameSuffix = "-rc1"
             resValue("string", "application_name", "Neo Store")
             manifestPlaceholders["appIcon"] = "@mipmap/ic_launcher"
             manifestPlaceholders["appIconRound"] = "@mipmap/ic_launcher_round"
