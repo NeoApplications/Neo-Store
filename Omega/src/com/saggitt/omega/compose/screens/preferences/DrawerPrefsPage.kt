78594fa4d97dcff4367d77b03efeec9ad7dcc9e2
Saul <saul_henriquez@hotmail.com>
2022-08-09 10:19:53 -0600

    Add save scroll position preference
    Signed-off-by: Saul <saul_henriquez@hotmail.com>

--- Omega/src/com/saggitt/omega/compose/screens/preferences/DrawerPrefsPage.kt
+++ Omega/src/com/saggitt/omega/compose/screens/preferences/DrawerPrefsPage.kt
@@ -21,11 +21,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -61,7 +57,8 @@ fun DrawerPrefsPage() {
         prefs.drawerSortMode, // TODO
         // TODO missing categorization pref page
         prefs.drawerSeparateWorkApps,
-        prefs.drawerCellHeightMultiplier
+        prefs.drawerCellHeightMultiplier,
+        prefs.drawerSaveScrollPosition
     )
     val searchPrefs = listOf(
         // TODO move to Search?
