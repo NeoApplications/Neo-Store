fc677898fdd3d4d376864a83d6ec094ee8b3d6fe
Saul <saul_henriquez@hotmail.com>
2022-08-12 11:36:38 -0600

    Add database 5_by_5 and 6_by_6
    Signed-off-by: Saul <saul_henriquez@hotmail.com>

--- src/com/android/launcher3/LauncherFiles.java
+++ src/com/android/launcher3/LauncherFiles.java
@@ -17,15 +17,16 @@ public class LauncherFiles {
     public static final String LAUNCHER_DB = "launcher.db";
     public static final String LAUNCHER_DB_CUSTOM = "launcher_\\d_\\d_\\d.db$";
     public static final String LAUNCHER_DB2 = "NeoLauncher.db";
+    public static final String LAUNCHER_6_BY_5_DB = "launcher_6_by_5.db";
+    public static final String LAUNCHER_5_BY_5_DB = "launcher_5_by_5.db";
     public static final String LAUNCHER_4_BY_5_DB = "launcher_4_by_5.db";
     public static final String LAUNCHER_4_BY_4_DB = "launcher_4_by_4.db";
     public static final String LAUNCHER_3_BY_3_DB = "launcher_3_by_3.db";
     public static final String LAUNCHER_2_BY_2_DB = "launcher_2_by_2.db";
     public static final String BACKUP_DB = "backup.db";
-    public static final String SHARED_PREFERENCES_KEY = BuildConfig.APPLICATION_ID + "_preferences";
+    public static final String SHARED_PREFERENCES_KEY = "com.android.launcher3.prefs";
     public static final String OLD_SHARED_PREFERENCES_KEY = "com.android.launcher3.prefs";
-    public static final String MANAGED_USER_PREFERENCES_KEY =
-            "com.android.launcher3.managedusers.prefs";
+    public static final String MANAGED_USER_PREFERENCES_KEY = "com.android.launcher3.managedusers.prefs";
     // This preference file is not backed up to cloud.
     public static final String DEVICE_PREFERENCES_KEY = "com.android.launcher3.device.prefs";
 
@@ -34,6 +35,8 @@ public class LauncherFiles {
 
     public static final List<String> ALL_FILES = Collections.unmodifiableList(Arrays.asList(
             LAUNCHER_DB,
+            LAUNCHER_6_BY_5_DB,
+            LAUNCHER_5_BY_5_DB,
             LAUNCHER_4_BY_5_DB,
             LAUNCHER_4_BY_4_DB,
             LAUNCHER_3_BY_3_DB,
