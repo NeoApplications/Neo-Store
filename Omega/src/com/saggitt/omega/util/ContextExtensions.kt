26b3e6aca1cd538b0f52395ce80cc3c1c179d1f4
Saul Henriquez <saul_henriquez@hotmail.com>
2022-08-13 08:34:43 -0600

    Move Language options to ContextExtensions
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

--- Omega/src/com/saggitt/omega/util/ContextExtensions.kt
+++ Omega/src/com/saggitt/omega/util/ContextExtensions.kt
@@ -24,8 +24,11 @@ import android.content.pm.PackageManager
 import android.graphics.drawable.Drawable
 import androidx.annotation.ColorInt
 import androidx.core.content.ContextCompat
+import com.android.launcher3.BuildConfig
 import com.android.launcher3.Launcher
 import com.android.launcher3.Utilities
+import com.saggitt.omega.PREFS_LANGUAGE_DEFAULT_CODE
+import com.saggitt.omega.PREFS_LANGUAGE_DEFAULT_NAME
 import com.saggitt.omega.preferences.OmegaPreferences
 import java.util.*
 
@@ -91,4 +94,70 @@ val Context.locale: Locale
 
 fun <T> useApplicationContext(creator: (Context) -> T): (Context) -> T {
     return { it -> creator(it.applicationContext) }
+}
+
+
+fun Context.languageOptions(): Map<String, String> {
+
+    val langCodes = BuildConfig.DETECTED_ANDROID_LOCALES
+    val languages: ArrayList<String> = ArrayList()
+    val contextUtils = Config(this)
+
+    if (langCodes.isNotEmpty()) {
+        for (langId in langCodes) {
+            val locale: Locale = contextUtils.getLocaleByAndroidCode(langId)
+            languages.add(summarizeLocale(locale, langId) + ";" + langId)
+        }
+    }
+
+    // Sort languages naturally
+    languages.sort()
+
+    val mEntries = arrayOfNulls<String>(languages.size + 2)
+    val mEntryValues = arrayOfNulls<String>(languages.size + 2)
+
+    for (i in languages.indices) {
+        mEntries[i + 2] = languages[i].split(";").toTypedArray()[0]
+        mEntryValues[i + 2] = languages[i].split(";").toTypedArray()[1]
+    }
+
+    mEntryValues[0] = ""
+    mEntries[0] = "$PREFS_LANGUAGE_DEFAULT_NAME » " + summarizeLocale(
+        resources.configuration.locales.get(0), ""
+    )
+
+    mEntryValues[1] = PREFS_LANGUAGE_DEFAULT_CODE
+    mEntries[1] = summarizeLocale(
+        contextUtils.getLocaleByAndroidCode(PREFS_LANGUAGE_DEFAULT_CODE),
+        PREFS_LANGUAGE_DEFAULT_NAME
+    )
+
+    return mEntryValues.filterNotNull()
+        .zip(mEntries.filterNotNull())
+        .toMap()
+}
+
+private fun summarizeLocale(locale: Locale, localeAndroidCode: String): String {
+    val country = locale.getDisplayCountry(locale)
+    val language = locale.getDisplayLanguage(locale)
+    var ret = (locale.getDisplayLanguage(Locale.ENGLISH)
+        .toString() + " (" + language.substring(0, 1)
+        .uppercase(Locale.getDefault()) + language.substring(1)
+            + (if (country.isNotEmpty() && country.lowercase(Locale.getDefault()) != language.lowercase(
+            Locale.getDefault()
+        )
+    ) ", $country" else "")
+            + ")")
+    if (localeAndroidCode == "zh-rCN") {
+        ret = ret.substring(
+            0,
+            ret.indexOf(" ") + 1
+        ) + "Simplified" + ret.substring(ret.indexOf(" "))
+    } else if (localeAndroidCode == "zh-rTW") {
+        ret = ret.substring(
+            0,
+            ret.indexOf(" ") + 1
+        ) + "Traditional" + ret.substring(ret.indexOf(" "))
+    }
+    return ret
 }
