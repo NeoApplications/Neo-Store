710f67e413e863ccce841bd226bf39d72f9102a5
Saul Henriquez <saul_henriquez@hotmail.com>
2022-08-13 18:47:34 -0600

    Add DevPrefPage
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

---
+++ Omega/src/com/saggitt/omega/compose/screens/preferences/DevPrefPage.kt
@@ -0,0 +1,72 @@
+/*
+ * This file is part of Neo Launcher
+ * Copyright (c) 2022   Neo Launcher Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.saggitt.omega.compose.screens.preferences
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.android.launcher3.R
+import com.android.launcher3.Utilities
+import com.saggitt.omega.compose.components.ViewWithActionBar
+import com.saggitt.omega.compose.components.preferences.PreferenceBuilder
+import com.saggitt.omega.theme.OmegaAppTheme
+
+@Composable
+fun DevPrefPage() {
+    val context = LocalContext.current
+    val prefs = Utilities.getOmegaPrefs(context)
+    val openDialog = remember { mutableStateOf(false) }
+    var dialogPref by remember { mutableStateOf<Any?>(null) }
+    val onPrefDialog = { pref: Any ->
+        dialogPref = pref
+        openDialog.value = true
+    }
+
+    val developerPrefs = listOf(
+        //pref.restartLauncher TODO
+        prefs.developerOptionsEnabled,
+        prefs.showDebugInfo
+    )
+
+    OmegaAppTheme {
+        ViewWithActionBar(
+            title = stringResource(R.string.developer_options_title)
+        ) {
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(
+                    start = 8.dp,
+                    end = 8.dp,
+                    top = 48.dp,
+                    bottom = 8.dp
+                ),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                item {
+                    developerPrefs.forEach { PreferenceBuilder(it, onPrefDialog) }
+                }
+            }
+        }
+    }
+}
