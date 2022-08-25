35f5815af14d2705d7bb6653d057b9aa1b56d2a8
machiav3lli <antonios.hazim@tutanota.com>
2022-08-25 21:09:31 +0200

    Update: Stable icon size for multi selection item

--- Omega/src/com/saggitt/omega/compose/components/preferences/SelectionPrefDialogUI.kt
+++ Omega/src/com/saggitt/omega/compose/components/preferences/SelectionPrefDialogUI.kt
@@ -24,6 +24,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -294,6 +295,7 @@ fun StringMultiSelectionPrefDialogUI(
                     ) {
                         if (pref.withIcons) iconIds[item.first]?.let {
                             Icon(
+                                modifier = Modifier.size(32.dp),
                                 painter = painterResource(id = it),
                                 contentDescription = stringResource(id = item.second),
                             )
