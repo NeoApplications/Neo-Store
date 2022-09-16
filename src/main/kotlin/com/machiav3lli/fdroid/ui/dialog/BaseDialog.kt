3a67085da16c5d7fe6dbb853241aaa9d26c0ae7e
machiav3lli <antonios.hazim@tutanota.com>
2022-09-16 16:39:08 +0200

    Add: BaseDialog composable

---
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/dialog/BaseDialog.kt
@@ -0,0 +1,21 @@
+package com.machiav3lli.fdroid.ui.dialog
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun BaseDialog(
+    openDialogCustom: MutableState<Boolean>,
+    dialogUI: @Composable (() -> Unit)
+) {
+    Dialog(
+        onDismissRequest = { openDialogCustom.value = false },
+        properties = DialogProperties(usePlatformDefaultWidth = false)
+    ) {
+        dialogUI()
+    }
+}
