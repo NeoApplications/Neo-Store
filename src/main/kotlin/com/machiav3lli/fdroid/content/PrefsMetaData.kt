83bedddf39aec84ad99f1094a6aaaf2d77d02304
machiav3lli <antonios.hazim@tutanota.com>
2022-09-19 00:52:52 +0200

    Update: Allow setting higher number for updated/new apps' recycler

--- src/main/kotlin/com/machiav3lli/fdroid/content/PrefsMetaData.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/content/PrefsMetaData.kt
@@ -81,8 +81,8 @@ val PrefsEntries = mapOf(
 )
 
 val IntPrefsRanges = mapOf(
-    Preferences.Key.UpdatedApps to 1..200,
-    Preferences.Key.NewApps to 1..50,
+    Preferences.Key.UpdatedApps to 1..400,
+    Preferences.Key.NewApps to 1..100,
     Preferences.Key.AutoSyncInterval to null,
     Preferences.Key.ReleasesCacheRetention to 0..365,
     Preferences.Key.ImagesCacheRetention to 0..365,
