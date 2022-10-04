f239d640524783bd0952f5b3fc1df24882e01bdb
Saul <saul_henriquez@hotmail.com>
2022-10-03 22:28:34 -0600

    add remember to vars
    Signed-off-by: Saul <saul_henriquez@hotmail.com>

--- Omega/src/com/saggitt/omega/folder/CustomizeFolderSheet.kt
+++ Omega/src/com/saggitt/omega/folder/CustomizeFolderSheet.kt
@@ -20,11 +20,30 @@ package com.saggitt.omega.folder
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
@@ -96,11 +115,11 @@ fun CustomizeFolderView(
     defaultTitle: String,
 ) {
     val keyboardController = LocalSoftwareKeyboardController.current
-    val coverMode = mutableStateOf(folder.isCoverMode)
+    val coverMode = remember { mutableStateOf(folder.isCoverMode) }
     val swipeUpHandler = createGestureHandler(
         launcher, folder.swipeUpAction, BlankGestureHandler(launcher, null)
     )
-    val handlerName = mutableStateOf(swipeUpHandler.displayName)
+    val handlerName = remember { mutableStateOf(swipeUpHandler.displayName) }
 
     Column(
         modifier = Modifier
