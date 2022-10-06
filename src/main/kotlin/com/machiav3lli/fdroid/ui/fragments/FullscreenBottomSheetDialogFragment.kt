416399e5236847c03546491442d75d692b0358c2
machiav3lli <antonios.hazim@tutanota.com>
2022-10-06 02:38:16 +0200

    Update: Improve BottomSheet's creation

--- src/main/kotlin/com/machiav3lli/fdroid/ui/fragments/FullscreenBottomSheetDialogFragment.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/fragments/FullscreenBottomSheetDialogFragment.kt
@@ -3,7 +3,6 @@ package com.machiav3lli.fdroid.ui.fragments
 import android.app.Dialog
 import android.os.Bundle
 import android.view.View
-import android.view.WindowManager
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
@@ -11,17 +10,7 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 abstract class FullscreenBottomSheetDialogFragment : BottomSheetDialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val sheet = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
-        sheet.setOnShowListener {
-            val parentLayout =
-                sheet.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
-            parentLayout?.let { view ->
-                val behaviour = BottomSheetBehavior.from(view)
-                val layoutParams = view.layoutParams
-                layoutParams.height = WindowManager.LayoutParams.MATCH_PARENT
-                view.layoutParams = layoutParams
-                behaviour.state = BottomSheetBehavior.STATE_EXPANDED
-            }
-        }
+        sheet.behavior.state = BottomSheetBehavior.STATE_EXPANDED
         return sheet
     }
 
