be30ac9bcddefd7afa54dcda7d3851c65f651621
machiav3lli <antonios.hazim@tutanota.com>
2022-10-15 13:10:07 +0200

    Update: Revamp MusicBar layout

--- Omega/src/com/saggitt/omega/compose/components/MusicBar.kt
+++ Omega/src/com/saggitt/omega/compose/components/MusicBar.kt
@@ -4,10 +4,20 @@ import android.media.AudioManager
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.FilledTonalIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -35,15 +45,34 @@ fun MusicBar(
             if (playing) MusicControlItem.PAUSE else MusicControlItem.PLAY,
             MusicControlItem.NEXT
         ).forEach {
-            CardButton(
-                modifier = Modifier.weight(1f),
-                icon = painterResource(id = it.icon),
-                description = stringResource(id = it.description),
-                ratio = ratio / 2.8f,
+            FilledTonalIconButton(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .weight(1f)
+                    .aspectRatio(ratio / 2.8f),
+                shape = MaterialTheme.shapes.medium,
+                colors = IconButtonDefaults.filledIconButtonColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(1.dp),
+                    contentColor = MaterialTheme.colorScheme.primary
+                ),
                 onClick = {
                     it.onClick(audioManager)
                     play(it != MusicControlItem.PAUSE)
-                })
+                }
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Icon(
+                        modifier = Modifier
+                            .fillMaxHeight(0.5f)
+                            .aspectRatio(1f),
+                        painter = painterResource(id = it.icon),
+                        contentDescription = stringResource(id = it.description)
+                    )
+                }
+            }
         }
     }
 }
