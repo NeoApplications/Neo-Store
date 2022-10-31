25c87392392092dbe76634f6658d836298953d59
machiav3lli <antonios.hazim@tutanota.com>
2022-10-31 01:47:17 +0100

    Add: Plus & QrCode phosphor icons

---
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/compose/icons/phosphor/Plus.kt
@@ -0,0 +1,52 @@
+package com.machiav3lli.backup.ui.compose.icons.phosphor
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType.Companion.NonZero
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap.Companion.Butt
+import androidx.compose.ui.graphics.StrokeJoin.Companion.Miter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.ImageVector.Builder
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+import com.machiav3lli.fdroid.ui.compose.icons.Phosphor
+
+val Phosphor.Plus: ImageVector
+    get() {
+        if (_plus != null) {
+            return _plus!!
+        }
+        _plus = Builder(
+            name = "Plus",
+            defaultWidth = 32.0.dp,
+            defaultHeight = 32.0.dp,
+            viewportWidth = 256.0f,
+            viewportHeight = 256.0f
+        ).apply {
+            path(
+                fill = SolidColor(Color(0xFF000000)), stroke = null, strokeLineWidth = 0.0f,
+                strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                pathFillType = NonZero
+            ) {
+                moveTo(224.0f, 128.0f)
+                arcToRelative(8.0f, 8.0f, 0.0f, false, true, -8.0f, 8.0f)
+                horizontalLineTo(136.0f)
+                verticalLineToRelative(80.0f)
+                arcToRelative(8.0f, 8.0f, 0.0f, false, true, -16.0f, 0.0f)
+                verticalLineTo(136.0f)
+                horizontalLineTo(40.0f)
+                arcToRelative(8.0f, 8.0f, 0.0f, false, true, 0.0f, -16.0f)
+                horizontalLineToRelative(80.0f)
+                verticalLineTo(40.0f)
+                arcToRelative(8.0f, 8.0f, 0.0f, false, true, 16.0f, 0.0f)
+                verticalLineToRelative(80.0f)
+                horizontalLineToRelative(80.0f)
+                arcTo(8.0f, 8.0f, 0.0f, false, true, 224.0f, 128.0f)
+                close()
+            }
+        }
+            .build()
+        return _plus!!
+    }
+
+private var _plus: ImageVector? = null
---
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/compose/icons/phosphor/QrCode.kt
@@ -0,0 +1,111 @@
+package com.machiav3lli.fdroid.ui.compose.icons.phosphor
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType.Companion.NonZero
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap.Companion.Butt
+import androidx.compose.ui.graphics.StrokeJoin.Companion.Miter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.ImageVector.Builder
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+import com.machiav3lli.fdroid.ui.compose.icons.Phosphor
+
+val Phosphor.QrCode: ImageVector
+    get() {
+        if (_qr_code != null) {
+            return _qr_code!!
+        }
+        _qr_code = Builder(
+            name = "Qr-code",
+            defaultWidth = 32.0.dp,
+            defaultHeight = 32.0.dp,
+            viewportWidth = 256.0f,
+            viewportHeight = 256.0f
+        ).apply {
+            path(
+                fill = SolidColor(Color(0xFF000000)), stroke = null, strokeLineWidth = 0.0f,
+                strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                pathFillType = NonZero
+            ) {
+                moveTo(104.0f, 40.0f)
+                lineTo(56.0f, 40.0f)
+                arcTo(16.0f, 16.0f, 0.0f, false, false, 40.0f, 56.0f)
+                verticalLineToRelative(48.0f)
+                arcToRelative(16.0f, 16.0f, 0.0f, false, false, 16.0f, 16.0f)
+                horizontalLineToRelative(48.0f)
+                arcToRelative(16.0f, 16.0f, 0.0f, false, false, 16.0f, -16.0f)
+                lineTo(120.0f, 56.0f)
+                arcTo(16.0f, 16.0f, 0.0f, false, false, 104.0f, 40.0f)
+                close()
+                moveTo(104.0f, 104.0f)
+                lineTo(56.0f, 104.0f)
+                lineTo(56.0f, 56.0f)
+                horizontalLineToRelative(48.0f)
+                verticalLineToRelative(48.0f)
+                close()
+                moveTo(104.0f, 136.0f)
+                lineTo(56.0f, 136.0f)
+                arcToRelative(16.0f, 16.0f, 0.0f, false, false, -16.0f, 16.0f)
+                verticalLineToRelative(48.0f)
+                arcToRelative(16.0f, 16.0f, 0.0f, false, false, 16.0f, 16.0f)
+                horizontalLineToRelative(48.0f)
+                arcToRelative(16.0f, 16.0f, 0.0f, false, false, 16.0f, -16.0f)
+                lineTo(120.0f, 152.0f)
+                arcTo(16.0f, 16.0f, 0.0f, false, false, 104.0f, 136.0f)
+                close()
+                moveTo(104.0f, 200.0f)
+                lineTo(56.0f, 200.0f)
+                lineTo(56.0f, 152.0f)
+                horizontalLineToRelative(48.0f)
+                verticalLineToRelative(48.0f)
+                close()
+                moveTo(200.0f, 40.0f)
+                lineTo(152.0f, 40.0f)
+                arcToRelative(16.0f, 16.0f, 0.0f, false, false, -16.0f, 16.0f)
+                verticalLineToRelative(48.0f)
+                arcToRelative(16.0f, 16.0f, 0.0f, false, false, 16.0f, 16.0f)
+                horizontalLineToRelative(48.0f)
+                arcToRelative(16.0f, 16.0f, 0.0f, false, false, 16.0f, -16.0f)
+                lineTo(216.0f, 56.0f)
+                arcTo(16.0f, 16.0f, 0.0f, false, false, 200.0f, 40.0f)
+                close()
+                moveTo(200.0f, 104.0f)
+                lineTo(152.0f, 104.0f)
+                lineTo(152.0f, 56.0f)
+                horizontalLineToRelative(48.0f)
+                verticalLineToRelative(48.0f)
+                close()
+                moveTo(136.0f, 176.0f)
+                lineTo(136.0f, 144.0f)
+                arcToRelative(8.0f, 8.0f, 0.0f, false, true, 16.0f, 0.0f)
+                verticalLineToRelative(32.0f)
+                arcToRelative(8.0f, 8.0f, 0.0f, false, true, -16.0f, 0.0f)
+                close()
+                moveTo(216.0f, 160.0f)
+                arcToRelative(8.0f, 8.0f, 0.0f, false, true, -8.0f, 8.0f)
+                lineTo(184.0f, 168.0f)
+                verticalLineToRelative(40.0f)
+                arcToRelative(8.0f, 8.0f, 0.0f, false, true, -8.0f, 8.0f)
+                lineTo(144.0f, 216.0f)
+                arcToRelative(8.0f, 8.0f, 0.0f, false, true, 0.0f, -16.0f)
+                horizontalLineToRelative(24.0f)
+                lineTo(168.0f, 144.0f)
+                arcToRelative(8.0f, 8.0f, 0.0f, false, true, 16.0f, 0.0f)
+                verticalLineToRelative(8.0f)
+                horizontalLineToRelative(24.0f)
+                arcTo(8.0f, 8.0f, 0.0f, false, true, 216.0f, 160.0f)
+                close()
+                moveTo(216.0f, 192.0f)
+                verticalLineToRelative(16.0f)
+                arcToRelative(8.0f, 8.0f, 0.0f, false, true, -16.0f, 0.0f)
+                lineTo(200.0f, 192.0f)
+                arcToRelative(8.0f, 8.0f, 0.0f, false, true, 16.0f, 0.0f)
+                close()
+            }
+        }
+            .build()
+        return _qr_code!!
+    }
+
+private var _qr_code: ImageVector? = null
