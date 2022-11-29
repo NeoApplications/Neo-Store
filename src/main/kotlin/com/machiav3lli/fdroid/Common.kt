7bd3cd3e0d637108600888d9f1775f709b77a655
machiav3lli <antonios.hazim@tutanota.com>
2022-11-29 01:40:03 +0100

    Add: SourceInfo, TrackersGroup & PermissionsGroup entities

--- src/main/kotlin/com/machiav3lli/fdroid/Common.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/Common.kt
@@ -79,6 +79,9 @@ const val HELP_TELEGRAM = "https://t.me/neo_android_store"
 const val HELP_MATRIX = "https://matrix.to/#/#neo-store:matrix.org"
 const val HELP_LICENSE = "https://github.com/NeoApplications/Neo-Store/blob/master/COPYING"
 
+const val PERMISSION_GROUP_INTERNET = "android.permission-group.INTERNET"
+const val PERMISSION_READ_CELL_BROADCASTS = "android.permission.READ_CELL_BROADCASTS"
+
 const val NAV_MAIN = 0
 const val NAV_PREFS = 1
 
---
+++ src/main/kotlin/com/machiav3lli/fdroid/entity/Privacy.kt
@@ -0,0 +1,189 @@
+package com.machiav3lli.fdroid.entity
+
+import androidx.annotation.StringRes
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.machiav3lli.backup.ui.compose.icons.phosphor.GitPullRequest
+import com.machiav3lli.fdroid.PERMISSION_GROUP_INTERNET
+import com.machiav3lli.fdroid.R
+import com.machiav3lli.fdroid.ui.compose.icons.Icon
+import com.machiav3lli.fdroid.ui.compose.icons.Phosphor
+import com.machiav3lli.fdroid.ui.compose.icons.icon.Opensource
+import com.machiav3lli.fdroid.ui.compose.icons.phosphor.AddressBook
+import com.machiav3lli.fdroid.ui.compose.icons.phosphor.Broadcast
+import com.machiav3lli.fdroid.ui.compose.icons.phosphor.Bug
+import com.machiav3lli.fdroid.ui.compose.icons.phosphor.Calendar
+import com.machiav3lli.fdroid.ui.compose.icons.phosphor.ChartLine
+import com.machiav3lli.fdroid.ui.compose.icons.phosphor.Chat
+import com.machiav3lli.fdroid.ui.compose.icons.phosphor.Copyleft
+import com.machiav3lli.fdroid.ui.compose.icons.phosphor.Copyright
+import com.machiav3lli.fdroid.ui.compose.icons.phosphor.CurrencyCircleDollar
+import com.machiav3lli.fdroid.ui.compose.icons.phosphor.EyeSlash
+import com.machiav3lli.fdroid.ui.compose.icons.phosphor.FolderNotch
+import com.machiav3lli.fdroid.ui.compose.icons.phosphor.Globe
+import com.machiav3lli.fdroid.ui.compose.icons.phosphor.MapPin
+import com.machiav3lli.fdroid.ui.compose.icons.phosphor.Microphone
+import com.machiav3lli.fdroid.ui.compose.icons.phosphor.Phone
+import com.machiav3lli.fdroid.ui.compose.icons.phosphor.ShieldStar
+import com.machiav3lli.fdroid.ui.compose.icons.phosphor.User
+import com.machiav3lli.fdroid.ui.compose.icons.phosphor.UserFocus
+
+open class PermissionGroup(
+    val name: String,
+    @StringRes val labelId: Int,
+    val icon: ImageVector,
+) {
+    object Contacts : PermissionGroup(
+        android.Manifest.permission_group.CONTACTS,
+        R.string.permission_contacts,
+        Phosphor.AddressBook
+    )
+
+    object Calendar : PermissionGroup(
+        android.Manifest.permission_group.CALENDAR,
+        R.string.permission_calendar,
+        Phosphor.Calendar
+    )
+
+    object SMS : PermissionGroup(
+        android.Manifest.permission_group.SMS,
+        R.string.permission_sms,
+        Phosphor.Chat
+    )
+
+    object Storage : PermissionGroup(
+        android.Manifest.permission_group.STORAGE,
+        R.string.permission_storage,
+        Phosphor.FolderNotch
+    )
+
+    object Phone : PermissionGroup(
+        android.Manifest.permission_group.PHONE,
+        R.string.permission_phone,
+        Phosphor.Phone
+    )
+
+    object Location : PermissionGroup(
+        android.Manifest.permission_group.LOCATION,
+        R.string.permission_location,
+        Phosphor.MapPin
+    )
+
+    object Camera : PermissionGroup(
+        android.Manifest.permission_group.CAMERA,
+        R.string.permission_contacts,
+        Phosphor.AddressBook
+    )
+
+    object Microphone : PermissionGroup(
+        android.Manifest.permission_group.MICROPHONE,
+        R.string.permission_microphone,
+        Phosphor.Microphone
+    )
+
+    object NearbyDevices : PermissionGroup(
+        android.Manifest.permission_group.NEARBY_DEVICES,
+        R.string.permission_nearby_devices,
+        Phosphor.Broadcast
+    )
+
+    object Internet : PermissionGroup(
+        PERMISSION_GROUP_INTERNET,
+        R.string.permission_internet,
+        Phosphor.Globe
+    )
+
+    object Other : PermissionGroup(
+        "",
+        R.string.permission_other,
+        Phosphor.ShieldStar
+    )
+
+    companion object {
+        fun String.getPermissionGroup() = when (this) {
+            android.Manifest.permission_group.CONTACTS -> Contacts
+            android.Manifest.permission_group.CALENDAR -> Calendar
+            android.Manifest.permission_group.SMS -> SMS
+            android.Manifest.permission_group.STORAGE -> Storage
+            android.Manifest.permission_group.PHONE -> Phone
+            android.Manifest.permission_group.MICROPHONE -> Microphone
+            android.Manifest.permission_group.LOCATION -> Location
+            android.Manifest.permission_group.CAMERA -> Camera
+            android.Manifest.permission_group.NEARBY_DEVICES -> NearbyDevices
+            PERMISSION_GROUP_INTERNET -> Internet
+            else -> Other
+        }
+    }
+}
+
+open class TrackersGroup(
+    @StringRes labelId: Int,
+    @StringRes descriptionId: Int,
+    icon: ImageVector,
+) {
+    object Identification : TrackersGroup(
+        R.string.trackers_identification,
+        R.string.trackers_identification_description,
+        Phosphor.User
+    )
+
+    object Analytics : TrackersGroup(
+        R.string.trackers_analytics,
+        R.string.trackers_analytics_description,
+        Phosphor.ChartLine
+    )
+
+    object Ads : TrackersGroup(
+        R.string.trackers_ads,
+        R.string.trackers_ads_description,
+        Phosphor.CurrencyCircleDollar
+    )
+
+    object Profiling : TrackersGroup(
+        R.string.trackers_profiling,
+        R.string.trackers_profiling_description,
+        Phosphor.UserFocus
+    )
+
+    object BugReporting : TrackersGroup(
+        R.string.trackers_bug,
+        R.string.trackers_bug_description,
+        Phosphor.Bug
+    )
+}
+
+open class SourceInfo(
+    @StringRes labelId: Int,
+    @StringRes descriptionId: Int,
+    icon: ImageVector,
+) {
+    object Proprietary : SourceInfo(
+        R.string.source_proprietary,
+        R.string.source_proprietary_description,
+        Phosphor.EyeSlash
+    )
+
+    object Open : SourceInfo(
+        R.string.source_open,
+        R.string.source_open_description,
+        Icon.Opensource
+    )
+
+    object Copyleft : SourceInfo(
+        R.string.source_copyleft,
+        R.string.source_copyleft_description,
+        Phosphor.Copyleft
+    )
+
+    object Copyright : SourceInfo(
+        R.string.source_copyright,
+        R.string.source_copyright_description,
+        Phosphor.Copyright
+    )
+
+    object Dependency : SourceInfo(
+        R.string.source_dependencies,
+        R.string.source_dependencies_description,
+        Phosphor.GitPullRequest
+    )
+
+}
