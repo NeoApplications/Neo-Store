bc6086bc19dde73281af59c4851ec33cd334edf1
machiav3lli <antonios.hazim@tutanota.com>
2022-12-26 17:44:06 +0100

    Add: Vertical modifier

--- src/main/kotlin/com/machiav3lli/fdroid/ui/compose/utils/Modifier.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/compose/utils/Modifier.kt
@@ -2,6 +2,7 @@ package com.machiav3lli.fdroid.ui.compose.utils
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.layout
 
 @Composable
 inline fun Modifier.addIf(
@@ -9,3 +10,14 @@ inline fun Modifier.addIf(
     crossinline factory: @Composable Modifier.() -> Modifier
 ): Modifier =
     if (condition) factory() else this
+
+fun Modifier.vertical() =
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+        layout(placeable.height, placeable.width) {
+            placeable.place(
+                x = -(placeable.width / 2 - placeable.height / 2),
+                y = -(placeable.height / 2 - placeable.width / 2)
+            )
+        }
+    }
