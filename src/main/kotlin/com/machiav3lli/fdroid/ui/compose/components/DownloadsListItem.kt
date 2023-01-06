1b860902b29a4b481cf564a93fdbd27dfc5ff8bf
machiav3lli <antonios.hazim@tutanota.com>
2023-01-06 03:21:14 +0100

    Add: DownloadList item composable

---
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/compose/components/DownloadsListItem.kt
@@ -0,0 +1,98 @@
+package com.machiav3lli.fdroid.ui.compose.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.machiav3lli.fdroid.database.entity.Installed
+import com.machiav3lli.fdroid.database.entity.Repository
+import com.machiav3lli.fdroid.entity.ProductItem
+import com.machiav3lli.fdroid.network.CoilDownloader
+import com.machiav3lli.fdroid.service.DownloadService
+import com.machiav3lli.fdroid.ui.compose.components.appsheet.DownloadProgress
+import com.machiav3lli.fdroid.ui.compose.utils.NetworkImage
+
+@Composable
+fun DownloadsListItem(
+    item: ProductItem,
+    repo: Repository? = null,
+    installed: Installed? = null,
+    state: DownloadService.State,
+    onUserClick: (ProductItem) -> Unit = {},
+) {
+    val product by remember(item) { mutableStateOf(item) }
+    val imageData by remember(product, repo) {
+        mutableStateOf(
+            CoilDownloader.createIconUri(
+                product.packageName,
+                product.icon,
+                product.metadataIcon,
+                repo?.address,
+                repo?.authentication
+            ).toString()
+        )
+    }
+
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .clickable { onUserClick(product) },
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        NetworkImage(
+            modifier = Modifier.size(64.dp),
+            data = imageData
+        )
+
+        Column(
+            modifier = Modifier
+                .weight(1f, true)
+                .height(64.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = product.name,
+                    modifier = Modifier
+                        .weight(1f),
+                    softWrap = true,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    style = MaterialTheme.typography.titleMedium
+                )
+                /*Text( TODO add version to download state and differentiate updates
+                    text = state.version,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    style = MaterialTheme.typography.titleSmall,
+                    fontWeight = FontWeight.ExtraBold
+                )*/
+            }
+            DownloadProgress(
+                modifier = Modifier.padding(horizontal = 12.dp),
+                totalSize = if (state is DownloadService.State.Downloading) state.total
+                    ?: 1L else 1L,
+                isIndeterminate = state !is DownloadService.State.Downloading,
+                downloaded = if (state is DownloadService.State.Downloading) state.read else 0L,
+            )
+        }
+    }
+}
