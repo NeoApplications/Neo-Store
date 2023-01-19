92a45235f5daaae2f7529798006be1faae38710a
machiav3lli <antonios.hazim@tutanota.com>
2023-01-19 15:50:29 +0100

    Update: Make Extras serializable

--- src/main/kotlin/com/machiav3lli/fdroid/database/entity/Extras.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/database/entity/Extras.kt
@@ -3,12 +3,23 @@ package com.machiav3lli.fdroid.database.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.machiav3lli.fdroid.TABLE_EXTRAS_NAME
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 @Entity(tableName = TABLE_EXTRAS_NAME)
+@Serializable
 data class Extras(
     @PrimaryKey
     var packageName: String = "",
     var favorite: Boolean = false,
     var ignoreUpdates: Boolean = false,
     var ignoredVersion: Long = 0L,
-)
+) {
+    fun toJSON() = Json.encodeToString(this)
+
+    companion object {
+        fun fromJson(json: String) = Json.decodeFromString<Extras>(json)
+    }
+}
