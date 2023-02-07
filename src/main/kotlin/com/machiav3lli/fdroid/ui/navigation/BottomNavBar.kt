c04eada71355a083c75500b73599394efdbe42ff
machiav3lli <antonios.hazim@tutanota.com>
2023-02-07 02:02:57 +0100

    Update: Revamp BottomNavBar button

--- src/main/kotlin/com/machiav3lli/fdroid/ui/navigation/BottomNavBar.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/navigation/BottomNavBar.kt
@@ -18,6 +18,12 @@
 package com.machiav3lli.fdroid.ui.navigation
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -25,14 +31,14 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -50,7 +56,7 @@ fun BottomNavBar(page: Int = NAV_MAIN, navController: NavHostController) {
             NavItem.ReposPrefs,
             NavItem.OtherPrefs,
         )
-        else -> listOf(
+        else      -> listOf(
             NavItem.Explore,
             NavItem.Latest,
             NavItem.Installed,
@@ -69,38 +75,9 @@ fun BottomNavBar(page: Int = NAV_MAIN, navController: NavHostController) {
         items.forEach { item ->
             val selected = currentDestination?.contains(item.destination) ?: false
 
-            NavigationBarItem(
-                icon = {
-                    Icon(
-                        imageVector = item.icon,
-                        contentDescription = stringResource(id = item.title),
-                        modifier = Modifier
-                            .background(
-                                if (selected) MaterialTheme.colorScheme.surfaceColorAtElevation(48.dp)
-                                else Color.Transparent,
-                                CircleShape
-                            )
-                            .padding(8.dp)
-                            .size(if (selected) 36.dp else 26.dp),
-                    )
-                },
-                label = {
-                    if (!selected)
-                        Text(
-                            text = stringResource(id = item.title),
-                            style = MaterialTheme.typography.bodyMedium,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
-                        )
-                },
-                colors = NavigationBarItemDefaults.colors(
-                    indicatorColor = MaterialTheme.colorScheme.background,
-                    selectedIconColor = MaterialTheme.colorScheme.primary,
-                    selectedTextColor = MaterialTheme.colorScheme.primary,
-                    unselectedIconColor = MaterialTheme.colorScheme.onSurface,
-                    unselectedTextColor = MaterialTheme.colorScheme.onSurface,
-                ),
-                alwaysShowLabel = true,
+            NavBarItem(
+                icon = item.icon,
+                labelId = item.title,
                 selected = selected,
                 onClick = {
                     navController.navigate(item.destination) {
@@ -115,6 +92,55 @@ fun BottomNavBar(page: Int = NAV_MAIN, navController: NavHostController) {
                     }
                 }
             )
+        }
+    }
+}
+
+@Composable
+fun RowScope.NavBarItem(
+    modifier: Modifier = Modifier,
+    icon: ImageVector,
+    labelId: Int,
+    selected: Boolean,
+    onClick: () -> Unit = {},
+) {
+    Row(
+        modifier = Modifier
+            .clickable { onClick() }
+            .weight(1f),
+        horizontalArrangement = Arrangement.Center,
+    ) {
+        Column(
+            modifier = modifier.fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = stringResource(id = labelId),
+                modifier = Modifier
+                    .background(
+                        if (selected) MaterialTheme.colorScheme.surfaceColorAtElevation(48.dp)
+                        else Color.Transparent,
+                        CircleShape
+                    )
+                    .padding(8.dp)
+                    .size(if (selected) 36.dp else 26.dp),
+                tint = if (selected) MaterialTheme.colorScheme.primary
+                else MaterialTheme.colorScheme.onSurface
+            )
+            if (!selected) Text(
+                text = stringResource(id = labelId),
+                style = MaterialTheme.typography.bodyMedium,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                color = MaterialTheme.colorScheme.onSurface,
+            )
+            /*NavigationBarItemDefaults.colors(
+                indicatorColor = MaterialTheme.colorScheme.background,
+                selectedIconColor = MaterialTheme.colorScheme.primary,
+                unselectedIconColor = MaterialTheme.colorScheme.onSurface,
+            )*/
         }
     }
 }
