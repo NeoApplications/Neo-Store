15e8ca0aabb61b144b1d369b22eaa2e26ee125c7
machiav3lli <antonios.hazim@tutanota.com>
2023-03-01 02:43:21 +0100

    Add: Downloaded item composable

--- src/main/kotlin/com/machiav3lli/fdroid/ui/compose/components/DownloadsListItem.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/compose/components/DownloadsListItem.kt
@@ -19,6 +19,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import com.machiav3lli.fdroid.database.entity.Downloaded
+import com.machiav3lli.fdroid.database.entity.IconDetails
 import com.machiav3lli.fdroid.database.entity.Installed
 import com.machiav3lli.fdroid.database.entity.Repository
 import com.machiav3lli.fdroid.entity.ProductItem
@@ -95,6 +97,84 @@ fun DownloadsListItem(
                     ?: 1L else 1L,
                 isIndeterminate = state !is DownloadService.State.Downloading,
                 downloaded = if (state is DownloadService.State.Downloading) state.read else 0L,
+            )
+        }
+    }
+}
+
+@Composable
+fun DownloadedItem(
+    item: Downloaded,
+    iconDetails: IconDetails?,
+    repo: Repository? = null,
+    installed: Installed? = null,
+    state: DownloadService.State,
+    onUserClick: (Downloaded) -> Unit = {},
+) {
+    val download by remember(item) { mutableStateOf(item) }
+    val imageData by remember(download, repo) {
+        mutableStateOf(
+            CoilDownloader.createIconUri(
+                download.packageName,
+                iconDetails?.icon ?: "",
+                iconDetails?.metadataIcon ?: "",
+                repo?.address,
+                repo?.authentication
+            ).toString()
+        )
+    }
+
+    Row(
+        modifier = Modifier
+            .clip(MaterialTheme.shapes.large)
+            .clickable { onUserClick(download) }
+            .padding(8.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        NetworkImage(
+            modifier = Modifier.size(48.dp),
+            data = imageData
+        )
+
+        Column(
+            modifier = Modifier
+                .weight(1f, true)
+                .height(48.dp),
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+            ) {
+                Text(
+                    text = download.state.name,
+                    modifier = Modifier
+                        .weight(1f),
+                    softWrap = true,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Text(
+                    text = if (installed != null && installed.version != state.version) "${download.version} → ${state.version}"
+                    else state.version,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    style = MaterialTheme.typography.titleSmall,
+                )
+            }
+            DownloadProgress(
+                modifier = Modifier.padding(horizontal = 4.dp),
+                totalSize = if (state is DownloadService.State.Downloading) state.total
+                    ?: 1L else 1L,
+                isIndeterminate = state is DownloadService.State.Pending || state is DownloadService.State.Connecting,
+                downloaded = when (state) {
+                    is DownloadService.State.Downloading -> state.read
+                    is DownloadService.State.Success     -> 1L
+                    else                                 -> 0L
+                },
             )
         }
     }
