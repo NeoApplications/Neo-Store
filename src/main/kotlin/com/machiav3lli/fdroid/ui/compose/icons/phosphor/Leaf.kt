41a800b7b7b43832e33d73e7bca4389307bf997b
machiav3lli <antonios.hazim@tutanota.com>
2023-03-01 03:15:24 +0100

    Fix: Release build

--- src/main/kotlin/com/machiav3lli/fdroid/ui/compose/icons/phosphor/Leaf.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/compose/icons/phosphor/Leaf.kt
@@ -11,7 +11,6 @@ import androidx.compose.ui.graphics.StrokeJoin.Companion.Miter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.ImageVector.Builder
 import androidx.compose.ui.graphics.vector.path
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.machiav3lli.fdroid.ui.compose.icons.Phosphor
 
@@ -64,7 +63,7 @@ val Phosphor.Leaf: ImageVector
 private var _leaf: ImageVector? = null
 
 
-@Preview
+//@Preview
 @Composable
 fun LeafPreview() {
     Image(
