254ae3f26b4ae4f21b843a739c85720999edbee1
machiav3lli <antonios.hazim@tutanota.com>
2023-04-06 04:27:30 +0200

    Fix: Changed APIs after removed dependency

--- src/main/kotlin/com/machiav3lli/fdroid/ui/pages/ScreenshotsPage.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/pages/ScreenshotsPage.kt
@@ -1,7 +1,10 @@
 package com.machiav3lli.fdroid.ui.pages
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
@@ -10,14 +13,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.rememberPagerState
 import com.machiav3lli.fdroid.network.CoilDownloader
 import com.machiav3lli.fdroid.ui.compose.components.ScreenshotItem
 import com.machiav3lli.fdroid.ui.compose.utils.ZoomableImage
 
-@OptIn(ExperimentalPagerApi::class)
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ScreenshotsPage(
     screenshots: List<ScreenshotItem>,
@@ -25,7 +25,7 @@ fun ScreenshotsPage(
 ) {
     val pagerState = rememberPagerState()
 
-    HorizontalPager(count = screenshots.size, state = pagerState) { page ->
+    HorizontalPager(pageCount = screenshots.size, state = pagerState) { page ->
         val screenshot = screenshots[page]
         var image by remember { mutableStateOf<String?>(null) }
 
