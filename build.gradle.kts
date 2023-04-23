33b6e160f360e3d90000d82f1537eec72461777a
machiav3lli <antonios.hazim@tutanota.com>
2023-04-23 02:04:24 +0200

    Remove Dependency: Jetpack customview

--- build.gradle.kts
+++ build.gradle.kts
@@ -182,8 +182,6 @@ dependencies {
 
     debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
     debugImplementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
-    debugImplementation("androidx.customview:customview-poolingcontainer:1.0.0")
-    debugImplementation("androidx.customview:customview:1.2.0-alpha02")
 }
 
 // using a task as a preBuild dependency instead of a function that takes some time insures that it runs
