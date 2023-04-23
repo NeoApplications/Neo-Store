5cb385d4eeb88e3e667573d3c87023ffb7a5e194
machiav3lli <antonios.hazim@tutanota.com>
2023-04-23 02:21:14 +0200

    Remove: Scrollable top bars

--- src/main/kotlin/com/machiav3lli/fdroid/ui/activities/MainActivityX.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/activities/MainActivityX.kt
@@ -17,7 +17,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
@@ -29,7 +28,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavHostController
@@ -134,7 +132,6 @@ class MainActivityX : AppCompatActivity() {
                 }
                 val mScope = rememberCoroutineScope()
                 navController = rememberAnimatedNavController()
-                val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
                 var barVisible by remember { mutableStateOf(true) }
 
                 navController.addOnDestinationChangedListener { _, destination, _ ->
@@ -150,7 +147,6 @@ class MainActivityX : AppCompatActivity() {
                 }
 
                 Scaffold(
-                    modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
                     containerColor = Color.Transparent,
                     contentColor = MaterialTheme.colorScheme.onBackground,
                     bottomBar = {
@@ -163,10 +159,7 @@ class MainActivityX : AppCompatActivity() {
                         }
                     },
                     topBar = {
-                        TopBar(
-                            title = stringResource(id = R.string.application_name),
-                            scrollBehavior = scrollBehavior,
-                        ) {
+                        TopBar(title = stringResource(id = R.string.application_name)) {
                             AnimatedVisibility(barVisible) {
                                 ExpandableSearchAction(
                                     query = query,
--- src/main/kotlin/com/machiav3lli/fdroid/ui/activities/PrefsActivityX.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/activities/PrefsActivityX.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -23,7 +22,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavHostController
@@ -91,7 +89,6 @@ class PrefsActivityX : AppCompatActivity() {
                 }
             ) {
                 navController = rememberAnimatedNavController()
-                val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
                 var pageTitle: Int? by remember {
                     mutableStateOf(NavItem.Prefs.title)
                 }
@@ -101,17 +98,11 @@ class PrefsActivityX : AppCompatActivity() {
                 }
 
                 Scaffold(
-                    modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
                     containerColor = Color.Transparent,
                     contentColor = MaterialTheme.colorScheme.onBackground,
                     bottomBar = { BottomNavBar(page = NAV_PREFS, navController = navController) },
                     topBar = {
-                        TopBar(
-                            title = stringResource(
-                                id = pageTitle ?: NavItem.Prefs.title
-                            ),
-                            scrollBehavior = scrollBehavior
-                        )
+                        TopBar(title = stringResource(id = pageTitle ?: NavItem.Prefs.title))
                     }
                 ) { paddingValues ->
                     LaunchedEffect(key1 = navController) {
