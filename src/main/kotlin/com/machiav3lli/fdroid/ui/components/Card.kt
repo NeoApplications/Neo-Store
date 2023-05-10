79feb450662194772b97f432d45617b2707ec54b
machiav3lli <antonios.hazim@tutanota.com>
2023-05-10 14:27:37 +0200

    Update: Bind short description lines limit to extended state

--- src/main/kotlin/com/machiav3lli/fdroid/ui/components/Card.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/components/Card.kt
@@ -4,33 +4,33 @@ import androidx.compose.animation.*
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CornerBasedShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ExpandableCard(
     modifier: Modifier = Modifier,
-    preExpanded: Boolean = false,
+    isExpanded: MutableState<Boolean> = mutableStateOf(false),
     backgroundColor: Color = MaterialTheme.colorScheme.background,
     shape: CornerBasedShape = MaterialTheme.shapes.large,
     onClick: () -> Unit = {},
     expandedContent: @Composable () -> Unit = {},
     mainContent: @Composable () -> Unit,
 ) {
-    var expanded by rememberSaveable { mutableStateOf(preExpanded) }
     val background by animateColorAsState(
-        targetValue = if (expanded) MaterialTheme.colorScheme.surfaceVariant else backgroundColor
+        targetValue = if (isExpanded.value) MaterialTheme.colorScheme.surfaceVariant else backgroundColor
     )
 
     Surface(
@@ -39,15 +39,17 @@ fun ExpandableCard(
             .clip(shape)
             .combinedClickable(
                 onClick = onClick,
-                onLongClick = { expanded = !expanded }
+                onLongClick = { isExpanded.value = !isExpanded.value }
             ),
         color = background
     ) {
         Column(modifier = modifier, horizontalAlignment = Alignment.Start) {
             mainContent()
             AnimatedVisibility(
-                modifier = Modifier.align(Alignment.End),
-                visible = expanded,
+                modifier = Modifier
+                    .align(Alignment.End)
+                    .padding(8.dp),
+                visible = isExpanded.value,
                 enter = fadeIn() + expandIn(expandFrom = Alignment.TopEnd),
                 exit = fadeOut() + shrinkOut(shrinkTowards = Alignment.TopEnd)
             ) {
--- src/main/kotlin/com/machiav3lli/fdroid/ui/components/ProductsListItem.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/components/ProductsListItem.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -17,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -45,6 +45,7 @@ fun ProductsListItem(
     onActionClick: (ProductItem) -> Unit = {},
 ) {
     val product by remember(item) { mutableStateOf(item) }
+    val isExpanded = rememberSaveable { mutableStateOf(false) }
     val imageData by remember(product, repo) {
         mutableStateOf(
             CoilDownloader.createIconUri(
@@ -58,7 +59,7 @@ fun ProductsListItem(
     }
 
     ExpandableCard(
-        modifier = Modifier.padding(8.dp),
+        isExpanded = isExpanded,
         onClick = { onUserClick(product) },
         expandedContent = {
             ExpandedItemContent(
@@ -112,7 +113,7 @@ fun ProductsListItem(
                     text = product.summary,
                     style = MaterialTheme.typography.bodySmall,
                     overflow = TextOverflow.Ellipsis,
-                    maxLines = 2,
+                    maxLines = if (isExpanded.value) Int.MAX_VALUE else 2,
                     color = MaterialTheme.colorScheme.onSurfaceVariant
                 )
             },
