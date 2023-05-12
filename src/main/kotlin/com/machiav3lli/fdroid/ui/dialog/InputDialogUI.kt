4b9f37e3690f78412d99594b988bf2edfba1792a
machiav3lli <antonios.hazim@tutanota.com>
2023-05-12 23:37:02 +0200

    Add: StringInput DialogUI

--- src/main/kotlin/com/machiav3lli/fdroid/ui/dialog/InputDialogUI.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/dialog/InputDialogUI.kt
@@ -6,10 +6,15 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -17,6 +22,7 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -29,15 +35,21 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.machiav3lli.fdroid.R
 import com.machiav3lli.fdroid.content.IntPrefsRanges
 import com.machiav3lli.fdroid.content.NonBooleanPrefsMeta
 import com.machiav3lli.fdroid.content.Preferences
 import com.machiav3lli.fdroid.ui.components.DialogNegativeButton
 import com.machiav3lli.fdroid.ui.components.DialogPositiveButton
+import com.machiav3lli.fdroid.ui.compose.icons.Phosphor
+import com.machiav3lli.fdroid.ui.compose.icons.phosphor.X
+import com.machiav3lli.fdroid.utility.extension.text.RE_finishChars
 import kotlinx.coroutines.delay
 
 @Composable
@@ -195,6 +207,103 @@ fun StringInputPrefDialogUI(
                         openDialogCustom.value = false
                     }
                 )
+            }
+        }
+    }
+}
+
+const val DIALOG_NONE = 0
+
+@Composable
+fun StringInputDialogUI(
+    titleText: String,
+    initValue: String,
+    openDialogCustom: MutableState<Boolean>,
+    onSave: ((String) -> Unit) = {},
+) {
+    val context = LocalContext.current
+    val focusManager = LocalFocusManager.current
+    val mainFocusRequester = remember { FocusRequester() }
+
+    var savedValue by remember {
+        mutableStateOf(TextFieldValue(initValue, TextRange(initValue.length)))
+    }
+    var isEdited by remember { mutableStateOf(false) }
+    val scrollState = rememberScrollState()
+
+    fun submit() {
+        focusManager.clearFocus()
+        onSave(savedValue.text)
+        openDialogCustom.value = false
+    }
+
+    SideEffect { mainFocusRequester.requestFocus() }
+
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        modifier = Modifier.padding(8.dp),
+        elevation = CardDefaults.elevatedCardElevation(8.dp),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.background)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(text = titleText, style = MaterialTheme.typography.titleLarge)
+            Column(
+                modifier = Modifier
+                    .verticalScroll(scrollState)
+                    .fillMaxWidth()
+                    .weight(1f, false)
+            ) {
+                TextField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .focusRequester(mainFocusRequester),
+                    value = savedValue,
+                    colors = TextFieldDefaults.colors(
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedTextColor = MaterialTheme.colorScheme.onBackground,
+                        unfocusedTextColor = MaterialTheme.colorScheme.onBackground,
+                    ),
+                    shape = MaterialTheme.shapes.large,
+                    singleLine = true,
+                    onValueChange = {
+                        isEdited = true
+                        if (it.text.contains(RE_finishChars)) submit()
+                        else savedValue = it         // only save when no control char
+                    },
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        imeAction = ImeAction.Done,
+                        keyboardType = KeyboardType.Text,
+                    ),
+                    keyboardActions = KeyboardActions(onDone = { submit() }),
+                    trailingIcon = {
+                        Row {
+                            IconButton(onClick = { savedValue = TextFieldValue("") }) {
+                                Icon(
+                                    imageVector = Phosphor.X,
+                                    contentDescription = stringResource(id = R.string.clear_text),
+                                    tint = MaterialTheme.colorScheme.onSurface,
+                                )
+                            }
+                        }
+                    },
+                )
+            }
+
+            Row(
+                Modifier.fillMaxWidth()
+            ) {
+                DialogNegativeButton(textId = R.string.cancel) {
+                    openDialogCustom.value = false
+                }
+                Spacer(Modifier.weight(1f))
+                DialogPositiveButton(textId = R.string.save) {
+                    submit()
+                }
             }
         }
     }
