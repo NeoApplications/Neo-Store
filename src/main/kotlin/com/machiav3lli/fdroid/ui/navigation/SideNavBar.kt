bb0712a9f86bc7eea86e2052ee287d3618f8d021
machiav3lli <antonios.hazim@tutanota.com>
2023-05-12 13:24:14 +0200

    Update: Revamp SideNavBar

--- src/main/kotlin/com/machiav3lli/fdroid/ui/navigation/SideNavBar.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/navigation/SideNavBar.kt
@@ -21,23 +21,21 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.SelectableChipColors
 import androidx.compose.material3.Text
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
@@ -46,6 +44,7 @@ import androidx.compose.ui.unit.dp
 import com.machiav3lli.fdroid.entity.appCategoryIcon
 import com.machiav3lli.fdroid.ui.compose.utils.vertical
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SideNavBar(
     modifier: Modifier = Modifier,
@@ -59,10 +58,7 @@ fun SideNavBar(
         modifier = modifier
             .fillMaxHeight()
             .horizontalScroll(rememberScrollState()),
-        contentPadding = PaddingValues(
-            vertical = 12.dp,
-            horizontal = 4.dp,
-        ),
+        contentPadding = PaddingValues(vertical = 12.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp),
     ) {
         items(items) { item ->
@@ -83,57 +79,45 @@ fun SideNavBar(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SideNavBarItem(
     modifier: Modifier = Modifier,
     icon: ImageVector,
     label: String,
     selected: Boolean = true,
-    colors: SideNavBarItemColors = SideNavBarItemColors(
-        indicatorColor = MaterialTheme.colorScheme.surfaceColorAtElevation(48.dp),
-        selectedIconColor = MaterialTheme.colorScheme.primary,
-        selectedTextColor = MaterialTheme.colorScheme.primary,
-        unselectedIconColor = MaterialTheme.colorScheme.onSurface,
-        unselectedTextColor = MaterialTheme.colorScheme.onSurface,
+    colors: SelectableChipColors = FilterChipDefaults.filterChipColors(
+        containerColor = Color.Transparent,
+        selectedContainerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(48.dp),
+        selectedLeadingIconColor = MaterialTheme.colorScheme.primary,
+        selectedLabelColor = MaterialTheme.colorScheme.primary,
+        iconColor = MaterialTheme.colorScheme.onSurface,
+        labelColor = MaterialTheme.colorScheme.onSurface,
     ),
     alwaysShowIcon: Boolean = false,
     onClick: () -> Unit,
 ) {
-    Surface(
+    FilterChip(
+        modifier = modifier,
         shape = MaterialTheme.shapes.medium,
-        color = if (selected) colors.indicatorColor
-        else Color.Transparent,
+        border = null,
+        colors = colors,
         onClick = onClick,
-    ) {
-        Row(
-            modifier = modifier.padding(4.dp),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
+        selected = selected,
+        label = {
+            Text(
+                text = label,
+                maxLines = 1,
+            )
+        },
+        leadingIcon = {
             AnimatedVisibility(visible = selected || alwaysShowIcon) {
                 Icon(
                     modifier = Modifier,
                     imageVector = icon,
-                    tint = if (selected) colors.selectedIconColor
-                    else colors.unselectedIconColor,
                     contentDescription = label,
                 )
-                Spacer(modifier = Modifier.width(4.dp))
             }
-            Text(
-                text = label,
-                color = if (selected) colors.selectedTextColor
-                else colors.unselectedTextColor,
-                maxLines = 1,
-            )
         }
-    }
+    )
 }
-
-class SideNavBarItemColors(
-    val indicatorColor: Color,
-    val selectedIconColor: Color,
-    val selectedTextColor: Color,
-    val unselectedIconColor: Color,
-    val unselectedTextColor: Color,
-)
