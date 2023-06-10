f0f99b13f90f73fbea58b3643d1540ea9920dd94
machiav3lli <antonios.hazim@tutanota.com>
2023-06-11 01:55:07 +0200

    Add: DownloadWorker/-Manager related entities

---
+++ src/main/kotlin/com/machiav3lli/fdroid/service/works/DownloadEntity.kt
@@ -0,0 +1,109 @@
+package com.machiav3lli.fdroid.service.works
+
+import com.machiav3lli.fdroid.database.entity.Release
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+@Serializable
+class DownloadTask(
+    val started: Long,
+    val packageName: String,
+    val name: String,
+    val release: Release,
+    val url: String,
+    val repoId: Long,
+    val authentication: String,
+) {
+    val key: String
+        get() = "$packageName-$repoId-${release.version}"
+
+    fun toJSON() = Json.encodeToString(this)
+
+    companion object {
+        fun fromJson(json: String) = Json.decodeFromString<DownloadTask>(json)
+    }
+}
+
+@Serializable
+sealed class DownloadState {
+    abstract val packageName: String
+    abstract val name: String
+    abstract val version: String
+    abstract val cacheFileName: String
+    abstract val repoId: Long
+
+    @Serializable
+    class Pending(
+        override val packageName: String,
+        override val name: String,
+        override val version: String,
+        override val cacheFileName: String,
+        override val repoId: Long,
+        val blocked: Boolean,
+    ) : DownloadState()
+
+    @Serializable
+    class Connecting(
+        override val packageName: String,
+        override val name: String,
+        override val version: String,
+        override val cacheFileName: String,
+        override val repoId: Long,
+    ) : DownloadState()
+
+    @Serializable
+    class Downloading(
+        override val packageName: String,
+        override val name: String,
+        override val version: String,
+        override val cacheFileName: String,
+        override val repoId: Long,
+        val read: Long,
+        val total: Long?,
+    ) : DownloadState()
+
+    @Serializable
+    class Success(
+        override val packageName: String,
+        override val name: String,
+        override val version: String,
+        override val cacheFileName: String,
+        override val repoId: Long,
+        val release: Release,
+    ) : DownloadState()
+
+    @Serializable
+    class Error(
+        override val packageName: String,
+        override val name: String,
+        override val version: String,
+        override val cacheFileName: String,
+        override val repoId: Long,
+        val resultCode: Int,
+        val validationError: ValidationError,
+    ) : DownloadState()
+
+    @Serializable
+    class Cancel(
+        override val packageName: String,
+        override val name: String,
+        override val version: String,
+        override val cacheFileName: String,
+        override val repoId: Long,
+    ) : DownloadState()
+
+    fun toJSON() = Json.encodeToString(this)
+
+    companion object {
+        fun fromJson(json: String) = Json.decodeFromString<DownloadState>(json)
+    }
+}
+
+enum class ValidationError { NONE, INTEGRITY, FORMAT, METADATA, SIGNATURE, PERMISSIONS }
+
+sealed class ErrorType {
+    object Network : ErrorType()
+    object Http : ErrorType()
+    class Validation(val validateError: ValidationError) : ErrorType()
+}
