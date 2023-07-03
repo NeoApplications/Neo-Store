c113fc8253207962ffca36cc36f6ff181535a798
machiav3lli <antonios.hazim@tutanota.com>
2023-07-03 02:22:02 +0200

    Update: Replace pages with pager in NavHosts

--- src/main/kotlin/com/machiav3lli/fdroid/ui/navigation/NavHost.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/navigation/NavHost.kt
@@ -1,38 +1,31 @@
 package com.machiav3lli.fdroid.ui.navigation
 
 import androidx.compose.animation.AnimatedVisibilityScope
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.pager.PagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NamedNavArgument
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.activity
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.navArgument
-import com.machiav3lli.fdroid.MainApplication
-import com.machiav3lli.fdroid.content.Preferences
-import com.machiav3lli.fdroid.pages.ExplorePage
-import com.machiav3lli.fdroid.pages.InstalledPage
-import com.machiav3lli.fdroid.pages.LatestPage
 import com.machiav3lli.fdroid.pages.PermissionsPage
-import com.machiav3lli.fdroid.pages.PrefsOtherPage
-import com.machiav3lli.fdroid.pages.PrefsPersonalPage
-import com.machiav3lli.fdroid.pages.PrefsReposPage
-import com.machiav3lli.fdroid.pages.PrefsUpdatesPage
 import com.machiav3lli.fdroid.ui.activities.PrefsActivityX
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MainNavHost(
     modifier: Modifier = Modifier,
     navController: NavHostController,
+    pagerState: PagerState,
+    pages: List<NavItem>,
 ) =
     NavHost(
         modifier = modifier,
@@ -41,65 +34,40 @@ fun MainNavHost(
     ) {
         fadeComposable(NavItem.Permissions.destination) {
             PermissionsPage {
-                navController.navigate(Preferences[Preferences.Key.DefaultTab].valueString)
+                navController.navigate(NavItem.Main.destination)
             }
         }
-        fadeComposable(NavItem.Explore.destination) {
-            val viewModel = MainApplication.mainActivity?.exploreViewModel!!
-            ExplorePage(viewModel)
-        }
-        fadeComposable(route = NavItem.Latest.destination) {
-            val viewModel = MainApplication.mainActivity?.latestViewModel!!
-            LatestPage(viewModel)
-        }
-        fadeComposable(NavItem.Installed.destination) {
-            val viewModel = MainApplication.mainActivity?.installedViewModel!!
-            InstalledPage(viewModel)
+        fadeComposable(NavItem.Main.destination) {
+            SlidePager(
+                pagerState = pagerState,
+                pageItems = pages,
+                navController = navController
+            )
         }
         activity(NavItem.Prefs.destination) {
             this.activityClass = PrefsActivityX::class
         }
     }
 
-@OptIn(ExperimentalAnimationApi::class)
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun PrefsNavHost(
     modifier: Modifier = Modifier,
     navController: NavHostController,
+    pagerState: PagerState,
+    pages: List<NavItem>,
 ) =
     NavHost(
         modifier = modifier,
         navController = navController,
-        startDestination = NavItem.PersonalPrefs.destination
+        startDestination = NavItem.Prefs.destination
     ) {
-        fadeComposable(NavItem.PersonalPrefs.destination) {
-            PrefsPersonalPage()
-        }
-        fadeComposable(NavItem.UpdatesPrefs.destination) {
-            PrefsUpdatesPage()
-        }
-        fadeComposable(
-            "${NavItem.ReposPrefs.destination}?address={address}?fingerprint={fingerprint}",
-            args = listOf(
-                navArgument("address") {
-                    type = NavType.StringType
-                    defaultValue = ""
-                },
-                navArgument("fingerprint") {
-                    type = NavType.StringType
-                    defaultValue = ""
-                },
+        fadeComposable(NavItem.Prefs.destination) {
+            SlidePager(
+                pagerState = pagerState,
+                pageItems = pages,
+                navController = navController
             )
-        ) {
-            val viewModel = MainApplication.prefsActivity?.prefsViewModel!!
-            val args = it.arguments!!
-            val address = args.getString("address") ?: ""
-            val fingerprint = args.getString("fingerprint")?.uppercase() ?: ""
-            PrefsReposPage(viewModel, address, fingerprint)
-        }
-        fadeComposable(NavItem.OtherPrefs.destination) {
-            val viewModel = MainApplication.prefsActivity?.prefsViewModel!!
-            PrefsOtherPage(viewModel)
         }
     }
 
