2a98b417c0897c5126706d44e88d872540e753e3
machiav3lli <antonios.hazim@tutanota.com>
2023-08-09 09:08:58 +0200

    Add: Page argument for Main navigation

--- src/main/kotlin/com/machiav3lli/fdroid/ui/navigation/NavHost.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/navigation/NavHost.kt
@@ -8,16 +8,21 @@ import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.navigation.NamedNavArgument
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.activity
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.machiav3lli.fdroid.content.Preferences
 import com.machiav3lli.fdroid.pages.PermissionsPage
 import com.machiav3lli.fdroid.ui.activities.PrefsActivityX
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -37,7 +42,20 @@ fun MainNavHost(
                 navController.navigate(NavItem.Main.destination)
             }
         }
-        fadeComposable(NavItem.Main.destination) {
+        fadeComposable(
+            "${NavItem.Main.destination}?page={page}",
+            args = listOf(
+                navArgument("page") {
+                    type = NavType.IntType
+                    defaultValue = Preferences[Preferences.Key.DefaultTab].valueString.toInt()
+                })
+        ) {
+            val scope = rememberCoroutineScope()
+            val args = it.arguments!!
+            val pi = args.getInt("page")
+            if (pi != Preferences[Preferences.Key.DefaultTab].valueString.toInt()) pagerState.apply {
+                scope.launch { scrollToPage(pi) }
+            }
             SlidePager(
                 pagerState = pagerState,
                 pageItems = pages,
