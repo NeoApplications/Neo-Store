f450bef0531216d740234ab10131656cd13d117c
machiav3lli <antonios.hazim@tutanota.com>
2023-09-25 23:44:51 +0200

    Update: Revamp screenshots page

--- src/main/kotlin/com/machiav3lli/fdroid/pages/ScreenshotsPage.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/pages/ScreenshotsPage.kt
@@ -1,38 +1,27 @@
 package com.machiav3lli.fdroid.pages
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.machiav3lli.fdroid.R
 import com.machiav3lli.fdroid.network.createScreenshotUri
 import com.machiav3lli.fdroid.ui.components.NetworkImage
 import com.machiav3lli.fdroid.ui.components.ScreenshotItem
-import com.machiav3lli.fdroid.ui.components.SecondaryActionButton
-import com.machiav3lli.fdroid.ui.compose.icons.Phosphor
-import com.machiav3lli.fdroid.ui.compose.icons.phosphor.ArrowCircleLeft
-import com.machiav3lli.fdroid.ui.compose.icons.phosphor.ArrowCircleRight
-import kotlinx.coroutines.launch
+import com.machiav3lli.fdroid.ui.compose.CarouselIndicators
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -40,18 +29,13 @@ fun ScreenshotsPage(
     screenshots: List<ScreenshotItem>,
     page: Int,
 ) {
-    val scope = rememberCoroutineScope()
     val pagerState = rememberPagerState(pageCount = { screenshots.size })
-    val currentPage by remember(pagerState.currentPage) {
-        mutableIntStateOf(pagerState.currentPage)
-    }
 
     Box(
         modifier = Modifier
             .fillMaxSize(),
         contentAlignment = Alignment.Center,
     ) {
-
         HorizontalPager(state = pagerState) { page ->
             val screenshot = screenshots[page]
             var image by remember { mutableStateOf<String?>(null) }
@@ -70,33 +54,15 @@ fun ScreenshotsPage(
                 contentScale = ContentScale.Fit,
             )
         }
-        Row(
+        CarouselIndicators(
             modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 16.dp)
                 .fillMaxWidth(),
-        ) {
-            Spacer(modifier = Modifier.width(12.dp))
-            AnimatedVisibility(
-                visible = currentPage != 0
-            ) {
-                SecondaryActionButton(
-                    icon = Phosphor.ArrowCircleLeft,
-                    description = stringResource(id = R.string.previous)
-                ) {
-                    scope.launch { pagerState.animateScrollToPage(currentPage - 1) }
-                }
-            }
-            Spacer(modifier = Modifier.weight(1f))
-            AnimatedVisibility(visible = currentPage < screenshots.size - 1) {
-                SecondaryActionButton(
-                    icon = Phosphor.ArrowCircleRight,
-                    description = stringResource(id = R.string.next)
-                ) {
-                    scope.launch { pagerState.animateScrollToPage(currentPage + 1) }
-                }
-            }
-            Spacer(modifier = Modifier.width(12.dp))
-        }
-        // TODO add indicators
+            size = screenshots.size,
+            dimension = 16.dp,
+            state = pagerState,
+        )
     }
 
     LaunchedEffect(page) {
