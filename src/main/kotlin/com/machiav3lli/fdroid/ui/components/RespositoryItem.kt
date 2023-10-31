0c8a65415b3151a25b225c0d4cb567c724728ba9
machiav3lli <antonios.hazim@tutanota.com>
2023-10-31 01:58:30 +0100

    Update: Repository item layout

--- src/main/kotlin/com/machiav3lli/fdroid/ui/components/RespositoryItem.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/components/RespositoryItem.kt
@@ -1,6 +1,5 @@
 package com.machiav3lli.fdroid.ui.components
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
@@ -19,9 +18,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import com.machiav3lli.backup.ui.compose.icons.phosphor.Plus
 import com.machiav3lli.fdroid.database.entity.Repository
 import com.machiav3lli.fdroid.ui.compose.icons.Phosphor
-import com.machiav3lli.fdroid.ui.compose.icons.phosphor.Check
+import com.machiav3lli.fdroid.ui.compose.icons.phosphor.X
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -75,13 +75,11 @@ fun RepositoryItem(
             }
         },
         trailingContent = {
-            AnimatedVisibility(visible = isEnabled) {
-                Icon(
-                    imageVector = Phosphor.Check,
-                    tint = MaterialTheme.colorScheme.primary,
-                    contentDescription = "Repository Enabled"
-                )
-            }
+            Icon(
+                imageVector = if (isEnabled) Phosphor.X else Phosphor.Plus,
+                tint = MaterialTheme.colorScheme.primary,
+                contentDescription = "Repository Enabled"
+            )
         }
     )
 }
