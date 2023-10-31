a6bbcb07dd7af073b5cf44b8c93104ebb62d1ca4
machiav3lli <antonios.hazim@tutanota.com>
2023-10-31 02:25:53 +0100

    Fix: Clip click animation to shape (closes #529)

--- src/main/kotlin/com/machiav3lli/fdroid/ui/navigation/BottomNavBar.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/navigation/BottomNavBar.kt
@@ -42,6 +42,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
@@ -202,6 +203,7 @@ fun RowScope.NavBarItem(
     Row(
         modifier = modifier
             .padding(vertical = 8.dp)
+            .clip(MaterialTheme.shapes.extraLarge)
             .clickable { onClick() }
             .background(
                 background,
