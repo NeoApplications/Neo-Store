061fbe2711c5bda8a6a3c7c2cf1d5ac8dc46029a
machiav3lli <antonios.hazim@tutanota.com>
2023-11-15 03:11:31 +0100

    Update: Use Ktor with OkHttp engine in ExodusRepo

--- src/main/kotlin/com/machiav3lli/fdroid/network/ExodusRepo.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/network/ExodusRepo.kt
@@ -7,8 +7,14 @@ import com.machiav3lli.fdroid.CLIENT_READ_TIMEOUT
 import com.machiav3lli.fdroid.CLIENT_WRITE_TIMEOUT
 import com.machiav3lli.fdroid.database.entity.ExodusData
 import com.machiav3lli.fdroid.database.entity.Trackers
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.client.request.url
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.isSuccess
 import org.koin.dsl.module
 import java.net.InetSocketAddress
 import java.net.Proxy
@@ -26,48 +32,52 @@ class RExodusAPI {
 
     private fun getProxy(onion: Boolean) = if (onion) onionProxy else proxy
 
-    val client = OkHttpClient.Builder()
-        .connectTimeout(CLIENT_CONNECT_TIMEOUT, TimeUnit.SECONDS)
-        .readTimeout(CLIENT_READ_TIMEOUT, TimeUnit.SECONDS)
-        .writeTimeout(CLIENT_WRITE_TIMEOUT, TimeUnit.SECONDS)
-        .proxy(getProxy(false))
-        .retryOnConnectionFailure(true)
-        .build()
+    val client = HttpClient(OkHttp) {
+        engine {
+            config {
+                connectTimeout(CLIENT_CONNECT_TIMEOUT, TimeUnit.SECONDS)
+                readTimeout(CLIENT_READ_TIMEOUT, TimeUnit.SECONDS)
+                writeTimeout(CLIENT_WRITE_TIMEOUT, TimeUnit.SECONDS)
+                proxy(getProxy(false))
+                retryOnConnectionFailure(true)
+            }
+        }
+    }
 
     companion object {
         const val URL_BASE = "https://reports.exodus-privacy.eu.org/api"
         const val AUTHENTICATION = "Token ${BuildConfig.KEY_API_EXODUS}"
     }
 
-    fun getTrackers(): Trackers {
-        val request = Request.Builder()
-            .url("$URL_BASE/trackers")
-            .header("Authorization", AUTHENTICATION)
-            .build()
+    suspend fun getTrackers(): Trackers {
+        val request = HttpRequestBuilder().apply {
+            url("$URL_BASE/trackers")
+            header("Authorization", AUTHENTICATION)
+        }
 
-        val result = client.newCall(request).execute()
-        if (!result.isSuccessful)
-            Log.w(this::javaClass.name, "getTrackers() failed: Response code  ${result.code}")
+        val result = client.get(request)
+        if (!result.status.isSuccess())
+            Log.w(this::javaClass.name, "getTrackers() failed: ${result.status}")
 
         return when {
-            result.isSuccessful -> Trackers.fromJson(result.body.string()) ?: Trackers()
-            else                -> Trackers()
+            result.status.isSuccess() -> Trackers.fromJson(result.bodyAsText()) ?: Trackers()
+            else                      -> Trackers()
         }
     }
 
-    fun getExodusInfo(packageName: String): List<ExodusData> {
-        val request = Request.Builder()
-            .url("$URL_BASE/search/$packageName/details")
-            .header("Authorization", AUTHENTICATION)
-            .build()
+    suspend fun getExodusInfo(packageName: String): List<ExodusData> {
+        val request = HttpRequestBuilder().apply {
+            url("$URL_BASE/search/$packageName/details")
+            header("Authorization", AUTHENTICATION)
+        }
 
-        val result = client.newCall(request).execute()
-        if (!result.isSuccessful)
-            Log.w(this::javaClass.name, "getExodusInfo() failed: Response code ${result.code}")
+        val result = client.get(request)
+        if (!result.status.isSuccess())
+            Log.w(this::javaClass.name, "getExodusInfo() failed: ${result.status}")
 
         return when {
-            result.isSuccessful -> ExodusData.listFromJson(result.body.string())
-            else                -> emptyList()
+            result.status.isSuccess() -> ExodusData.listFromJson(result.bodyAsText())
+            else                      -> emptyList()
         }
     }
 }
