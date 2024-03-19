6fd9f8dabbfdc86bd2d556d163dc4beb5bfc2ebc
RMKnight <rmknightingodsarmor87@gmail.com>
2024-03-19 05:09:56 -0500

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.SUCCESS_ON_PROJECT_REPOS)
     repositories {
         mavenCentral()
         google()
