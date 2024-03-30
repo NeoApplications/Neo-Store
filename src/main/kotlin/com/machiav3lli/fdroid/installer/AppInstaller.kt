ac3e0b2eeadcf11f4379b2a3c16e7563ed0124ab
Anna-Sophie Zaitsewa <toddot@protonmail.com>
2024-03-31 00:10:16 +0100

    Add System-wide installer based on priv-app permissions

--- src/main/kotlin/com/machiav3lli/fdroid/installer/AppInstaller.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/installer/AppInstaller.kt
@@ -1,6 +1,8 @@
 package com.machiav3lli.fdroid.installer
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import com.machiav3lli.fdroid.content.Preferences
 import com.machiav3lli.fdroid.entity.InstallerType
 import com.machiav3lli.fdroid.utility.amInstalled
@@ -13,6 +15,9 @@ abstract class AppInstaller {
         @Volatile
         private var INSTANCE: AppInstaller? = null
         fun getInstance(context: Context?): AppInstaller? {
+            val hasSystemInstallPermission = context?.packageManager?.checkPermission(
+                Manifest.permission.INSTALL_PACKAGES,
+                context.packageName) == PackageManager.PERMISSION_GRANTED;
             return INSTANCE ?: synchronized(this) {
                 context?.let { context ->
                     val instance = object : AppInstaller() {
@@ -20,6 +25,9 @@ abstract class AppInstaller {
                             get() {
                                 val installer = Preferences[Preferences.Key.Installer].installer
                                 return when {
+                                    hasSystemInstallPermission ->
+                                        SystemInstaller(context)
+
                                     installer == InstallerType.ROOT && shellIsRoot       ->
                                         RootInstaller(context)
 
