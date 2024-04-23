bbeaeaaaa81f702897513bd1d400ae37ee5f8604
machiav3lli <antonios.hazim@tutanota.com>
2024-04-23 18:38:01 +0200

    Fix: Tabs' button touch response

--- src/main/kotlin/com/machiav3lli/fdroid/ui/components/TabButton.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/components/TabButton.kt
@@ -9,17 +9,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TabPosition
 import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 
@@ -30,9 +31,10 @@ fun TabButton(
     icon: ImageVector? = null,
     onClick: () -> Unit,
 ) {
-    IconButton(
+    TextButton(
         modifier = modifier,
-        colors = IconButtonDefaults.iconButtonColors(
+        shape = RectangleShape,
+        colors = ButtonDefaults.textButtonColors(
             containerColor = Color.Transparent,
         ),
         onClick = onClick
