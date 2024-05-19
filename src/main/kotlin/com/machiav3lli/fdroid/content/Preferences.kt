5c7f3bd56ba8c7f54a4f98d557313abe7d6ad514
machiav3lli <antonios.hazim@tutanota.com>
2024-05-19 13:02:15 +0200

    Update: Increase default search apps to 2000

--- src/main/kotlin/com/machiav3lli/fdroid/content/Preferences.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/content/Preferences.kt
@@ -275,7 +275,7 @@ data object Preferences : OnSharedPreferenceChangeListener {
         data object BottomSearchBar : Key<Boolean>("bottom_search_bar", Value.BooleanValue(false))
 
         data object UpdatedApps : Key<Int>("updated_apps", Value.IntValue(150))
-        data object SearchApps : Key<Int>("search_apps", Value.IntValue(1000))
+        data object SearchApps : Key<Int>("search_apps", Value.IntValue(2000))
         data object NewApps : Key<Int>("new_apps", Value.IntValue(30))
 
         data object ProxyUrl : Key<String>("proxy_url", Value.StringValue(""))
