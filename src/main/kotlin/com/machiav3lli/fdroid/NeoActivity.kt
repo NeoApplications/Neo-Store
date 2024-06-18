799704d3b50af0ad49d1607d15b759eee050e043
machiav3lli <antonios.hazim@tutanota.com>
2024-06-18 02:23:10 +0200

    Fix: Missing import

--- src/main/kotlin/com/machiav3lli/fdroid/NeoActivity.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/NeoActivity.kt
@@ -43,6 +43,7 @@ import com.machiav3lli.fdroid.utility.extension.text.nullIfEmpty
 import com.machiav3lli.fdroid.utility.extension.text.pathCropped
 import com.machiav3lli.fdroid.utility.isBiometricLockEnabled
 import com.machiav3lli.fdroid.utility.isDarkTheme
+import com.machiav3lli.fdroid.viewmodels.AppSheetVM
 import com.machiav3lli.fdroid.viewmodels.ExploreVM
 import com.machiav3lli.fdroid.viewmodels.InstalledVM
 import com.machiav3lli.fdroid.viewmodels.LatestVM
