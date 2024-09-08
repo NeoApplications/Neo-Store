eb15bab96a50977ba9d99084f610c3821076d068
machiav3lli <antonios.hazim@tutanota.com>
2024-09-08 12:41:39 +0200

    Add kotlin.parcelize

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kotlin.parcelize)
     alias(libs.plugins.ksp)
 }
 
