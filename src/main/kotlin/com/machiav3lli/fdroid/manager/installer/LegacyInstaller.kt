050df0d2865b6dd640136da33e6f8d2fc05cddb3
machiav3lli <antonios.hazim@tutanota.com>
2025-06-04 01:19:44 +0200

    Update: Revamp LegacyInstaller improving its robustness

--- src/main/kotlin/com/machiav3lli/fdroid/manager/installer/LegacyInstaller.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/manager/installer/LegacyInstaller.kt
@@ -2,14 +2,9 @@ package com.machiav3lli.fdroid.manager.installer
 
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
-import android.os.Build
 import android.util.AndroidRuntimeException
-import com.machiav3lli.fdroid.NeoApp
-import com.machiav3lli.fdroid.data.content.Cache
-import com.machiav3lli.fdroid.data.content.Cache.getPackageArchiveInfo
 import com.machiav3lli.fdroid.data.content.Cache.getReleaseFileUri
-import com.machiav3lli.fdroid.utils.extension.android.Android
+import com.machiav3lli.fdroid.data.entity.InstallState
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.File
@@ -17,67 +12,86 @@ import java.io.File
 // TODO: Use this for MIUI device instead of guiding new users
 class LegacyInstaller(context: Context) : BaseInstaller(context) {
 
-    override suspend fun install(
-        packageLabel: String,
-        cacheFileName: String,
-        postInstall: () -> Unit
-    ) {
-        val cacheFile = Cache.getReleaseFile(context, cacheFileName)
-        val packageInfo = context.getPackageArchiveInfo(cacheFile)
-        val packageName = packageInfo?.packageName ?: "unknown-package"
+    override suspend fun processNextInstallation() {
+        val task = installQueue.getCurrentTask() ?: return
+        emitProgress(InstallState.Preparing, task.packageName)
 
-        mOldDefaultInstaller(packageName, cacheFile, postInstall)
-    }
-
-    override suspend fun isInstalling(packageName: String): Boolean =
-        NeoApp.enqueuedInstalls.contains(packageName)
-
-    override suspend fun uninstall(packageName: String) = mOldDefaultUninstaller(packageName)
-
-    private suspend fun mOldDefaultInstaller(
-        packageName: String,
-        cacheFile: File,
-        postInstall: () -> Unit
-    ) =
-        withContext(Dispatchers.IO) {
-            NeoApp.enqueuedInstalls.add(packageName)
-            val (uri, flags) = Pair(
-                Cache.getReleaseFile(context, cacheFile.name).getReleaseFileUri(context),
-                Intent.FLAG_GRANT_READ_URI_PERMISSION
+        val apkFile = getApkFile(task.cacheFileName) ?: run {
+            installQueue.onInstallationComplete(
+                Result.failure(InstallationError.Unknown("Installation failed: Failed to get APK file"))
             )
+            return
+        }
 
-            @Suppress("DEPRECATION")
-            try {
-                context.startActivity(
-                    Intent(Intent.ACTION_INSTALL_PACKAGE)
-                        .setDataAndType(uri, "application/vnd.android.package-archive")
-                        .setFlags(flags)
-                )
-            } catch (e: AndroidRuntimeException) {
-                context.startActivity(
-                    Intent(Intent.ACTION_INSTALL_PACKAGE)
-                        .setDataAndType(uri, "application/vnd.android.package-archive")
-                        .setFlags(flags or Intent.FLAG_ACTIVITY_NEW_TASK)
+        val packageName = extractPackageNameFromApk(apkFile) ?: task.packageName
+
+        withContext(Dispatchers.IO) {
+            runCatching {
+                installPackage(packageName, apkFile)
+                reportSuccess(packageName)
+            }.onFailure { e ->
+                installQueue.onInstallationComplete(
+                    Result.failure(InstallationError.Unknown("Installation failed: ${e.message}"))
                 )
             }
-            NeoApp.db.getInstallTaskDao().delete(packageName)
-            NeoApp.enqueuedInstalls.remove(packageName)
-            postInstall()
         }
+    }
 
-    private suspend fun mOldDefaultUninstaller(packageName: String) = withContext(Dispatchers.IO) {
-        val uri = Uri.fromParts("package", packageName, null)
-        val intent = Intent()
-        intent.data = uri
+    private suspend fun installPackage(packageName: String, apkFile: File) {
+        withContext(Dispatchers.IO) {
+            runCatching {
+                val intent = Intent(Intent.ACTION_INSTALL_PACKAGE).apply {
+                    data = apkFile.getReleaseFileUri(context)
+                    flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+                    // TODO to be tested
+                    putExtra(Intent.EXTRA_NOT_UNKNOWN_SOURCE, true)
+                    putExtra(Intent.EXTRA_INSTALLER_PACKAGE_NAME, context.packageName)
+                    putExtra(Intent.EXTRA_RETURN_RESULT, true)
+                    putExtra(
+                        "android.content.pm.extra.LEGACY_STATUS", // PackageInstaller.EXTRA_LEGACY_STATUS
+                        false
+                    )
+                }
 
-        @Suppress("DEPRECATION")
-        if (Android.sdk(Build.VERSION_CODES.P)) {
-            intent.action = Intent.ACTION_DELETE
-        } else {
-            intent.action = Intent.ACTION_UNINSTALL_PACKAGE
-            intent.putExtra(Intent.EXTRA_RETURN_RESULT, true)
+                // Can't track actual progress
+                emitProgress(InstallState.Installing(0.1f), packageName)
+                context.startActivity(intent)
+                reportSuccess(packageName)
+            }.onFailure { tr ->
+                when (tr) {
+                    is AndroidRuntimeException
+                         -> runCatching {
+                        val intent = Intent(Intent.ACTION_INSTALL_PACKAGE).apply {
+                            data = apkFile.getReleaseFileUri(context)
+                            flags =
+                                Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_ACTIVITY_NEW_TASK
+                            // TODO to be tested
+                            putExtra(Intent.EXTRA_NOT_UNKNOWN_SOURCE, true)
+                            putExtra(Intent.EXTRA_INSTALLER_PACKAGE_NAME, context.packageName)
+                            putExtra(Intent.EXTRA_RETURN_RESULT, true)
+                            putExtra(
+                                "android.content.pm.extra.LEGACY_STATUS", // PackageInstaller.EXTRA_LEGACY_STATUS
+                                false
+                            )
+                        }
+
+                        // Can't track actual progress
+                        emitProgress(InstallState.Installing(0.1f), packageName)
+                        context.startActivity(intent)
+                        reportSuccess(packageName)
+                    }.onFailure { e ->
+                        reportFailure(
+                            InstallationError.Unknown("Installation failed: Failed to start installation: ${e.message}"),
+                            packageName
+                        )
+                    }
+
+                    else -> reportFailure(
+                        InstallationError.Unknown("Installation failed: Failed to start installation: ${tr.message}"),
+                        packageName
+                    )
+                }
+            }
         }
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-        context.startActivity(intent)
     }
 }
