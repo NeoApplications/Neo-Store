0b2e71445934225f585ca06b2f80a60b126afc2f
machiav3lli <antonios.hazim@tutanota.com>
2025-06-04 01:50:24 +0200

    Update: Revamp SystemInstaller improving its robustness

--- src/main/kotlin/com/machiav3lli/fdroid/manager/installer/SystemInstaller.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/manager/installer/SystemInstaller.kt
@@ -3,106 +3,148 @@ package com.machiav3lli.fdroid.manager.installer
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageInstaller
-import android.net.Uri
-import android.os.Build
+import android.content.pm.PackageInstaller.SessionParams
 import android.util.Log
-import com.machiav3lli.fdroid.NeoApp
-import com.machiav3lli.fdroid.data.content.Cache
-import com.machiav3lli.fdroid.data.content.Cache.getPackageArchiveInfo
+import com.machiav3lli.fdroid.BUFFER_SIZE
 import com.machiav3lli.fdroid.data.content.Cache.getReleaseFileUri
-import com.machiav3lli.fdroid.utils.extension.android.Android
+import com.machiav3lli.fdroid.data.content.Preferences
+import com.machiav3lli.fdroid.data.entity.InstallState
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import kotlinx.io.IOException
+import kotlinx.io.files.FileNotFoundException
 import java.io.File
-import java.io.OutputStream
 
 class SystemInstaller(context: Context) : BaseInstaller(context) {
-    companion object{
-        private const val TAG: String = "SystemInstaller";
+
+    companion object {
+        private const val TAG: String = "SystemInstaller"
+
+        // TODO consider needing other flags
+        private val flags = PendingIntent.FLAG_IMMUTABLE
+
+        // TODO consider requiring extra params
+        private val params = SessionParams(SessionParams.MODE_FULL_INSTALL)
     }
-    private suspend fun mSystemInstaller(
-        packageName: String,
-        cacheFile: File,
-        postInstall: () -> Unit
-    ) =
+
+    private val packageInstaller = context.packageManager.packageInstaller
+
+    override suspend fun processNextInstallation() {
+        val task = installQueue.getCurrentTask() ?: return
+        emitProgress(InstallState.Preparing, task.packageName)
+
+        val apkFile = getApkFile(task.cacheFileName) ?: run {
+            installQueue.onInstallationComplete(
+                Result.failure(InstallationError.Unknown("Installation failed: Failed to get APK file"))
+            )
+            return
+        }
+
+        val packageName = extractPackageNameFromApk(apkFile) ?: task.packageName
+
         withContext(Dispatchers.IO) {
-            NeoApp.enqueuedInstalls.add(packageName)
-
-            val packageInstaller = context.packageManager.packageInstaller
-            val packageUri = Cache.getReleaseFile(context, cacheFile.name).getReleaseFileUri(context)
-            val params = PackageInstaller.SessionParams(PackageInstaller.SessionParams.MODE_FULL_INSTALL)
-            val sessionId = packageInstaller.createSession(params)
-            val session = packageInstaller.openSession(sessionId)
-
-            Log.d(TAG, "Installing: " + cacheFile.name)
-            Log.d(TAG, "packageUri: " + packageUri)
-
-            val out: OutputStream = session.openWrite("APPINSTALL", 0, -1)
-            val `in` = context.contentResolver.openInputStream(packageUri)
-
-            if (`in` == null){
-                Log.wtf(TAG, "package file provided can not be resolved")
-                return@withContext
-            }
-
-            `in`.copyTo(out)
-            session.fsync(out)
-            `in`.close()
-            out.close()
-
-            //TODO: Do we need to receive status somehow?
-            val intent = Intent()
-            val pendingIntent = PendingIntent.getActivity(context, 0, intent,
-                PendingIntent.FLAG_IMMUTABLE)
-            val statusReceiver = pendingIntent.intentSender
-
-            session.commit(statusReceiver)
-
-            NeoApp.db.getInstallTaskDao().delete(packageName)
-            NeoApp.enqueuedInstalls.remove(packageName)
-            postInstall()
-        }
-
-    private suspend fun mDefaultUninstaller(packageName: String) = withContext(Dispatchers.IO) {
-        val uri = Uri.fromParts("package", packageName, null)
-        val intent = Intent()
-        intent.data = uri
-
-        @Suppress("DEPRECATION")
-        when {
-            Android.sdk(Build.VERSION_CODES.P) -> {
-                intent.action = Intent.ACTION_DELETE
-            }
-
-            else                               -> {
-                intent.action = Intent.ACTION_UNINSTALL_PACKAGE
-                intent.putExtra(Intent.EXTRA_RETURN_RESULT, true)
+            try {
+                installPackage(packageName, apkFile)
+            } catch (e: Exception) {
+                installQueue.onInstallationComplete(
+                    Result.failure(InstallationError.Unknown("Installation failed: ${e.message}"))
+                )
             }
         }
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-
-        context.startActivity(intent)
     }
 
+    private suspend fun installPackage(packageName: String, apkFile: File) {
+        withContext(Dispatchers.IO) {
+            runCatching {
+                val packageUri = apkFile.getReleaseFileUri(context)
+                val sessionId = packageInstaller.createSession(params)
 
-    override suspend fun install(
-        packageLabel: String,
-        cacheFileName: String,
-        postInstall: () -> Unit
-    ) {
-        val cacheFile = Cache.getReleaseFile(context, cacheFileName)
-        val packageInfo = context.getPackageArchiveInfo(cacheFile)
-        val packageName = packageInfo?.packageName ?: "unknown-package"
+                // Open session and write APK file
+                packageInstaller.openSession(sessionId).use { session ->
+                    Log.d(TAG, "Installing: " + apkFile.name)
+                    Log.d(TAG, "packageUri: $packageUri")
 
-        mSystemInstaller(packageName, cacheFile, postInstall)
-    }
+                    session.openWrite("APPINSTALL", 0, -1)
+                        .use { out ->
+                            context.contentResolver.openInputStream(packageUri)
+                                .use { input ->
+                                    if (input == null) {
+                                        Log.wtf(TAG, "package file provided can not be resolved")
+                                        return@withContext
+                                    }
 
-    override suspend fun isInstalling(packageName: String): Boolean {
-        return NeoApp.enqueuedInstalls.contains(packageName)
-    }
+                                    // TODO consider adding listener to track progress
+                                    input.copyTo(out, BUFFER_SIZE)
+                                    session.fsync(out)
+                                }
+                        }
 
-    override suspend fun uninstall(packageName: String) {
-        mDefaultUninstaller(packageName)
+                    //TODO: Do we need to receive status somehow?
+                    // Consider using Intent(context, InstallerReceiver::class.java) instead
+                    val intent = Intent()
+                    val pendingIntent = PendingIntent.getActivity(
+                        context,
+                        sessionId,
+                        intent,
+                        flags
+                    )
+                    val statusReceiver = pendingIntent.intentSender
+
+                    emitProgress(InstallState.Installing(0.95f), packageName)
+                    session.commit(statusReceiver)
+                    if (Preferences[Preferences.Key.ReleasesCacheRetention] == 0) apkFile.delete()
+                    // TODO add receiver to track status
+                    reportSuccess(packageName)
+                }
+            }.onFailure { e ->
+                val errorMessage = when (e) {
+                    is SecurityException     -> {
+                        Log.w(
+                            "SessionInstaller",
+                            "Installation failed: Attempted to use a destroyed or sealed session when installing.\n${e.message}"
+                        )
+                        "Installation failed: Attempted to use a destroyed or sealed session when installing.\n${e.message}"
+                    }
+
+                    is FileNotFoundException -> {
+                        Log.w(
+                            "SessionInstaller",
+                            "Installation failed: Cache file does not seem to exist.\n${e.message}"
+                        )
+                        "Installation failed: Cache file does not seem to exist.\n${e.message}"
+                    }
+
+                    is IOException           -> {
+                        Log.w(
+                            "SessionInstaller",
+                            "Installation failed: Due to a bad pipe.\n${e.message}"
+                        )
+                        "Installation failed: Due to a bad pipe.\n${e.message}"
+                    }
+
+                    is IllegalStateException -> {
+                        Log.w(
+                            "SessionInstaller",
+                            "Installation failed: ${e.message}"
+                        )
+                        if (packageInstaller.mySessions.size == 50) {
+                            packageInstaller.mySessions
+                                .filter { it.isActive && it.appPackageName == context.packageName }
+                                .forEach {
+                                    packageInstaller.abandonSession(it.sessionId)
+                                }
+                            installPackage(packageName, apkFile)
+                            return@withContext
+                        } else throw e
+                    }
+
+                    else                     -> {
+                        throw e
+                    }
+                }
+
+                reportFailure(InstallationError.Unknown(errorMessage), packageName)
+            }
+        }
     }
 }
