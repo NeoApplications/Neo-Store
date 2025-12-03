362b063b6c9d654682ac35e92745c4f3f95a0320
machiav3lli <antonios.hazim@tutanota.com>
2025-12-03 22:32:31 +0100

    Update: Revamp Root installer for better performance and reliability

--- src/main/kotlin/com/machiav3lli/fdroid/manager/installer/type/RootInstaller.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/manager/installer/type/RootInstaller.kt
@@ -11,10 +11,7 @@ import com.machiav3lli.fdroid.utils.Utils.quotePath
 import com.machiav3lli.fdroid.utils.extension.android.Android
 import com.machiav3lli.fdroid.utils.notifyFinishedInstall
 import com.topjohnwu.superuser.Shell
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import kotlinx.io.IOException
 import java.io.File
@@ -23,27 +20,29 @@ import java.util.regex.Pattern
 class RootInstaller(context: Context) : BaseInstaller(context) {
 
     companion object {
-        private val getCurrentUserState: String =
+        private val sessionIdPattern = Pattern.compile("(\\d+)")
+
+        private fun String.quote() =
+            "\"${this.replace(Regex("""[\\$"`]""")) { c -> "\\${c.value}" }}\""
+
+        private val getCurrentUserState: String by lazy {
             if (Android.sdk(Build.VERSION_CODES.N_MR1)) Shell.cmd("am get-current-user")
                 .exec().out[0]
             else Shell.cmd("dumpsys activity | grep -E \"mUserLru\"")
                 .exec().out[0].trim()
                 .removePrefix("mUserLru: [").removeSuffix("]")
+        }
 
-        private val String.quote
-            get() = "\"${this.replace(Regex("""[\\$"`]""")) { c -> "\\${c.value}" }}\""
-
-        private val getUtilBoxPath: String
-            get() {
-                listOf("toybox", "busybox").forEach {
-                    val shellResult = Shell.cmd("which $it").exec()
-                    if (shellResult.out.isNotEmpty()) {
-                        val utilBoxPath = shellResult.out.joinToString("")
-                        if (utilBoxPath.isNotEmpty()) return utilBoxPath.quote
-                    }
+        private val getUtilBoxPath: String by lazy {
+            listOf("toybox", "busybox").forEach {
+                val shellResult = Shell.cmd("which $it").exec()
+                if (shellResult.out.isNotEmpty()) {
+                    val utilBoxPath = shellResult.out.joinToString("")
+                    if (utilBoxPath.isNotEmpty()) utilBoxPath.quote()
                 }
-                return ""
             }
+            ""
+        }
 
         fun File.legacyInstall() = listOfNotNull(
             "cat", quotePath(absolutePath),
@@ -94,89 +93,80 @@ class RootInstaller(context: Context) : BaseInstaller(context) {
         fun File.deletePackage(): String = listOfNotNull(
             getUtilBoxPath,
             "rm",
-            absolutePath.quote
+            absolutePath.quote()
         ).joinToString(" ")
     }
 
     override suspend fun installPackage(packageName: String, apkFile: File) {
-        withContext(Dispatchers.IO) {
-            try {
-                if (Preferences[Preferences.Key.RootSessionInstaller]) {
-                    Shell.cmd(apkFile.sessionInstallCreate())
-                        .submit {
-                            val sessionIdPattern = Pattern.compile("(\\d+)")
-                            val sessionIdMatcher =
-                                if (it.out.isNotEmpty()) sessionIdPattern.matcher(it.out[0])
-                                else null
-                            val found = sessionIdMatcher?.find() == true
-                            installQueue.emitProgress(InstallState.Installing(0.15f), packageName)
+        try {
+            if (Preferences[Preferences.Key.RootSessionInstaller])
+                installWithSession(packageName, apkFile)
+            else installLegacy(packageName, apkFile)
+        } catch (e: IOException) {
+            reportFailure(
+                InstallationError.Unknown("Installation failed: $e.message"),
+                packageName
+            )
+        }
+    }
 
-                            if (found) {
-                                val sessionId = sessionIdMatcher?.group(1)?.toInt() ?: -1
-                                Shell.cmd(apkFile.sessionInstallWrite(sessionId))
-                                    .submit {
-                                        installQueue.emitProgress(
-                                            InstallState.Installing(0.75f),
-                                            packageName
-                                        )
-                                        Shell.cmd(sessionInstallCommit(sessionId))
-                                            .submit { result ->
-                                                installQueue.emitProgress(
-                                                    InstallState.Installing(0.95f),
-                                                    packageName
-                                                )
-                                                runBlocking {
-                                                    if (result.isSuccess) reportSuccess(packageName)
-                                                    else reportFailure(
-                                                        InstallationError.Unknown(
-                                                            result.err.joinToString("\n")
-                                                        ),
-                                                        packageName
-                                                    )
-                                                }
-                                                if (result.isSuccess && Preferences[Preferences.Key.ReleasesCacheRetention] == 0)
-                                                    Shell.cmd(apkFile.deletePackage()).submit()
-                                                CoroutineScope(Dispatchers.Default).launch {
-                                                    notifyFinishedInstall(context, packageName)
-                                                }
-                                            }
-                                    }
-                            }
-                        }
-                } else {
-                    Shell.cmd(apkFile.legacyInstall())
-                        .submit { result ->
-                            if (result.isSuccess && Preferences[Preferences.Key.ReleasesCacheRetention] == 0)
-                                Shell.cmd(apkFile.deletePackage()).submit { result ->
-                                    installQueue.emitProgress(
-                                        InstallState.Installing(0.95f),
-                                        packageName
-                                    )
-                                    runBlocking {
-                                        if (result.isSuccess) reportSuccess(packageName)
-                                        else reportFailure(
-                                            InstallationError.Unknown(result.err.joinToString("\n")),
-                                            packageName
-                                        )
-                                    }
-                                    CoroutineScope(Dispatchers.Default).launch {
-                                        notifyFinishedInstall(context, packageName)
-                                    }
-                                }
-                        }
-                }
-            } catch (e: IOException) {
-                reportFailure(
-                    InstallationError.Unknown("Installation failed: $e.message"),
-                    packageName
-                )
+    private suspend fun installWithSession(packageName: String, apkFile: File) {
+        val createResult = Shell.cmd(apkFile.sessionInstallCreate()).exec()
+        val sessionIdMatcher = if (createResult.out.isEmpty()) null
+        else sessionIdPattern.matcher(createResult.out[0])
+        val found = sessionIdMatcher?.find() == true
+        installQueue.emitProgress(InstallState.Installing(0.15f), packageName)
+
+        if (found) {
+            val sessionId = sessionIdMatcher?.group(1)?.toInt() ?: -1
+            val writeResult = Shell.cmd(apkFile.sessionInstallWrite(sessionId)).exec()
+            installQueue.emitProgress(
+                InstallState.Installing(0.75f),
+                packageName
+            )
+            val commitResult = Shell.cmd(sessionInstallCommit(sessionId)).exec()
+            installQueue.emitProgress(
+                InstallState.Installing(0.95f),
+                packageName
+            )
+
+            if (commitResult.isSuccess) {
+                reportSuccess(packageName)
+                if (Preferences[Preferences.Key.ReleasesCacheRetention] == 0)
+                    Shell.cmd(apkFile.deletePackage()).submit()
+            } else reportFailure(
+                InstallationError.Unknown(commitResult.err.joinToString("\n")),
+                packageName
+            )
+            withContext(Dispatchers.Default) {
+                notifyFinishedInstall(context, packageName)
             }
+        }
+
+    }
+
+    private suspend fun installLegacy(packageName: String, apkFile: File) {
+        val installResult = Shell.cmd(apkFile.legacyInstall()).exec()
+        installQueue.emitProgress(
+            InstallState.Installing(0.95f),
+            packageName
+        )
+
+        if (installResult.isSuccess) {
+            reportSuccess(packageName)
+            if (Preferences[Preferences.Key.ReleasesCacheRetention] == 0) {
+                Shell.cmd(apkFile.deletePackage()).exec()
+            }
+        } else reportFailure(
+            InstallationError.Unknown(installResult.err.joinToString("\n")),
+            packageName
+        )
+        withContext(Dispatchers.Default) {
+            notifyFinishedInstall(context, packageName)
         }
     }
 
     override suspend fun uninstall(packageName: String) {
-        withContext(Dispatchers.IO) {
-            Shell.cmd(packageName.uninstall).submit()
-        }
+        Shell.cmd(packageName.uninstall).submit()
     }
 }
