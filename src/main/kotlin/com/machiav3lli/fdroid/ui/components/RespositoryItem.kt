80a0a8d3c9917e63b4a98a1b42a9e576d59915c8
machiav3lli <antonios.hazim@tutanota.com>
2026-01-03 23:34:36 +0100

    Add: Trusted-label to repository items

--- src/main/kotlin/com/machiav3lli/fdroid/ui/components/RespositoryItem.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/components/RespositoryItem.kt
@@ -3,7 +3,10 @@ package com.machiav3lli.fdroid.ui.components
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Badge
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
@@ -14,10 +17,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.machiav3lli.fdroid.R
 import com.machiav3lli.fdroid.data.database.entity.Repository
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -48,11 +55,22 @@ fun RepositoryItem(
             containerColor = backgroundColor,
         ),
         headlineContent = {
-            Text(
-                text = repository.name.trim(),
-                style = MaterialTheme.typography.titleMedium,
-                maxLines = 1
-            )
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    text = repository.name.trim(),
+                    style = MaterialTheme.typography.titleMedium,
+                    maxLines = 1
+                )
+                if (repository.trusted) Badge(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                ) {
+                    Text(stringResource(R.string.trusted_label))
+                }
+            }
         },
         supportingContent = {
             repository.description.trim().let {
