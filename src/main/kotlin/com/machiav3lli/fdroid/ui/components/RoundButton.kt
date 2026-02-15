669bea93b277bce711eb37ac1db8f419ec95a8d4
machiav3lli <antonios.hazim@tutanota.com>
2026-02-15 21:36:36 +0100

    Add: SyncButton composable

--- src/main/kotlin/com/machiav3lli/fdroid/ui/components/RoundButton.kt
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/components/RoundButton.kt
@@ -1,5 +1,11 @@
 package com.machiav3lli.fdroid.ui.components
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.padding
@@ -11,13 +17,19 @@ import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.machiav3lli.fdroid.R
+import com.machiav3lli.fdroid.ui.compose.icons.Phosphor
+import com.machiav3lli.fdroid.ui.compose.icons.phosphor.ArrowsClockwise
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -69,4 +81,40 @@ fun FilledRoundButton(
             modifier = Modifier.size(size)
         )
     }
+}
+
+@Composable
+fun SyncButton(
+    modifier: Modifier = Modifier,
+    isSyncing: Boolean = false,
+    onClick: () -> Unit = {},
+    onLongClick: () -> Unit = {},
+) {
+    val angle by animateFloatAsState(
+        if (isSyncing) {
+            val infiniteTransition = rememberInfiniteTransition(label = "infiniteTransition")
+
+            val animationProgress by infiniteTransition.animateFloat(
+                initialValue = 0f,
+                targetValue = 20f,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(
+                        durationMillis = 50000,
+                        easing = LinearEasing
+                    )
+                ), label = "animationProgress"
+            )
+            val angle = 360f * animationProgress
+            angle
+        } else 0f, label = "iconAngle"
+    )
+
+    RoundButton(
+        modifier = modifier
+            .graphicsLayer { rotationZ = angle },
+        icon = Phosphor.ArrowsClockwise,
+        description = stringResource(id = R.string.sync_repositories),
+        onLongClick = onLongClick,
+        onClick = onClick
+    )
 }
