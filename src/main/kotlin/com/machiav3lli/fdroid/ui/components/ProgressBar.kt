28e8e8fcc796ea8cd0c7a7e72b18ec5237a8971a
Sócrates Díaz <sdsxd1800@gmail.com>
2026-03-08 21:54:37 -0400

    feat: add progress indicators for search page

---
+++ src/main/kotlin/com/machiav3lli/fdroid/ui/components/ProgressBar.kt
@@ -0,0 +1,42 @@
+package com.machiav3lli.fdroid.ui.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import kotlinx.coroutines.delay
+
+data object DelayedLinearProgressBarDefaults {
+    const val SHORT_DELAY_MS: Long = 200L
+}
+
+@Composable
+fun DelayedLinearProgressBar(
+    visible: Boolean,
+    delayMillis: Long = DelayedLinearProgressBarDefaults.SHORT_DELAY_MS,
+) {
+    var show by remember { mutableStateOf(false) }
+    LaunchedEffect(visible) {
+        if (visible) {
+            delay(delayMillis)
+            show = true
+        } else {
+            show = false
+        }
+    }
+    AnimatedVisibility(
+        visible = show,
+        enter = expandVertically(),
+        exit = shrinkVertically(),
+    ) {
+        LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
+    }
+}
